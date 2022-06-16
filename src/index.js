7f13c7e22854822896e989cfd01c8dbc3c4bbd3d
fouinyla <mefouiny@gmail.com>
2022-06-16 18:19:24 +0300

    move routes to App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store/createStore";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import App from "./app/App";
-import Login from "./app/layouts/login";
-import NavBar from "./app/components/ui/navBar";
+
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
@@ -15,12 +14,7 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="login" element={<Login />}></Route>
-        <Route path="help" element={<p>Help</p>} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </Provider>
 );
