1fd1d5f6b9481d140646f08b2a45527ca0806898
Violetta <viol.fedoseenko@yandex.ru>
2022-06-23 01:41:08 +0300

    add: login form (email, password)

--- src/app/layouts/login/Login.js
+++ src/app/layouts/login/Login.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 
 const Login = () => {
   const [formType, setFormType] = useState("register");
+  const [data, setData]=useState({email:"", password:""})
+  const handleChange = ({target}) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value
+    }));
+  };
   const toggleFormType = () => {
     setFormType((prevState) =>
       prevState === "register" ? "login" : "register"
     );
   };
+
   return (
     <div>
       {formType === "register" ? (
@@ -20,7 +28,18 @@ const Login = () => {
       ) : (
         <>
           <h3>Вход</h3>
-
+          
+          <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" value={data.email} name="email" onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="password">Пароль</label>
+                <input type="password" id="password" value={data.password} name="password" onChange={handleChange}/>
+            </div>
+          </form>
+          
           <p>
             У Вас нет аккаунта?{" "}
             <button onClick={toggleFormType}>Создать</button>
