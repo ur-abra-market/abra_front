8f0ea6cb60c6d4d951e27c9cfd941c25e66053d8
Violetta <viol.fedoseenko@yandex.ru>
2022-06-24 16:52:24 +0300

    add: basic validation with validator.js

--- src/app/layouts/login/Login.js
+++ src/app/layouts/login/Login.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "../../components/commonComponents/textField";
+import { validator } from "../../utils/validator";
 
 const Login = () => {
   const [formType, setFormType] = useState("register");
-  const [data, setData]=useState({email:"", password:""})
+  const [data, setData]=useState({email:"", password:""});
+  const [errors, setErrors]=useState()
   const handleChange = ({target}) => {
     setData((prevState) => ({
       ...prevState,
       [target.name]: target.value
     }));
   };
+  const validetorConfig = {
+    email:{ isRequired: {message: "Email is required!"}},
+    password:{ isRequired: {message: "Password is required!"}},
+  }
+
   const toggleFormType = () => {
     setFormType((prevState) =>
       prevState === "register" ? "login" : "register"
     );
   };
+
+  useEffect(()=>{validate();},[data]);
+
+  const validate = () => {
+    const errors=validator(data, validetorConfig);
+    setErrors(errors);
+    return Object.keys(errors).length === 0
+  };
+
+  const handSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log(data);
+  }
 
   return (
     <div>
@@ -30,12 +52,13 @@ const Login = () => {
         <>
           <h3>Вход</h3>
           
-          <form action="">
+          <form action="" onSubmit={handSubmit} >
             <TextField 
               label="Email" 
               name="email" 
               value={data.email} 
               onChange={handleChange}
+              error={errors.email}
             />
             <TextField 
               label="Password" 
@@ -43,7 +66,9 @@ const Login = () => {
               name="password" 
               value={data.password} 
               onChange={handleChange}
+              error={errors.password}
             />
+            <button>Submit</button>
           </form>
 
           <p>
