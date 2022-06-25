5ca8ee8b63114bf4ba64f423977bfccd71973397
Violetta <viol.fedoseenko@yandex.ru>
2022-06-25 14:22:55 +0300

    refactor: transferring the authorization component to loginForm.js

--- src/app/layouts/login/Login.js
+++ src/app/layouts/login/Login.js
@@ -1,42 +1,15 @@
 import React, { useEffect, useState } from "react";
-import TextField from "../../components/commonComponents/textField";
-import { validator } from "../../utils/validator";
+import LoginForm from "../../components/ui/loginForm/loginForm";
 
 const Login = () => {
   const [formType, setFormType] = useState("register");
-  const [data, setData]=useState({email:"", password:""});
-  const [errors, setErrors]=useState()
-  const handleChange = ({target}) => {
-    setData((prevState) => ({
-      ...prevState,
-      [target.name]: target.value
-    }));
-  };
-  const validetorConfig = {
-    email:{ isRequired: {message: "Email is required!"}},
-    password:{ isRequired: {message: "Password is required!"}},
-  }
+
 
   const toggleFormType = () => {
     setFormType((prevState) =>
       prevState === "register" ? "login" : "register"
     );
   };
-
-  useEffect(()=>{validate();},[data]);
-
-  const validate = () => {
-    const errors=validator(data, validetorConfig);
-    setErrors(errors);
-    return Object.keys(errors).length === 0
-  };
-
-  const handSubmit = (e) => {
-    e.preventDefault();
-    const isValid = validate();
-    if (!isValid) return;
-    console.log(data);
-  }
 
   return (
     <div>
@@ -51,25 +24,8 @@ const Login = () => {
       ) : (
         <>
           <h3>Вход</h3>
-          
-          <form action="" onSubmit={handSubmit} >
-            <TextField 
-              label="Email" 
-              name="email" 
-              value={data.email} 
-              onChange={handleChange}
-              error={errors.email}
-            />
-            <TextField 
-              label="Password" 
-              type="password" 
-              name="password" 
-              value={data.password} 
-              onChange={handleChange}
-              error={errors.password}
-            />
-            <button>Submit</button>
-          </form>
+
+          {<LoginForm />}        
 
           <p>
             У Вас нет аккаунта?{" "}
