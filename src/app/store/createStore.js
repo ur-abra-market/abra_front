ac9763e9afe90eb65bbf6cdbd164b511ec6969a5
Beshil <ersanbievbesh@gmail.com>
2022-07-03 11:28:44 +0300

    file path changed

--- src/app/store/createStore.js
+++ src/app/store/createStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../../features/counter/counterSlice";
-import userReducer from "./users";
+import userReducer from "./user";
 
 export const store = configureStore({
   reducer: {
