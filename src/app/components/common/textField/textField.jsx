3ab8ed2e6898986228b300ac7f24a85358f1217a
Beshil <ersanbievbesh@gmail.com>
2022-07-12 16:27:11 +0300

    changed form on hook-form in loginForm

--- src/app/components/common/textField/textField.jsx
+++ src/app/components/common/textField/textField.jsx
@@ -1,57 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import style from "./textField.module.css"
-import eyeHiddenPassword from '../../../assets/img/icons/eye_hidden_password.png';
-import eyeVisiblePassword from '../../../assets/img/icons/eye_visible_password.png';
+import style from "./textField.module.css";
+import eyeHiddenPassword from "../../../assets/img/icons/eye_hidden_password.png";
+import eyeVisiblePassword from "../../../assets/img/icons/eye_visible_password.png";
 
-const TextField = ({label, type, name, value, onChange, error}) => {
-    const [showPassword, setShowPassword] = useState(false);
-    const toggleShowPassword = (e) => {
-        e.preventDefault();
-        setShowPassword((prevState) => !prevState);
-        let findPassword = document.getElementById('password');
-        findPassword.type = (!showPassword) ? "text" : "password";
-        console.log(findPassword.type);
-        console.log(showPassword);
-    }
-
-    return (
-        <>
-            <label htmlFor={name} className={style.labelTextField} > {label} </label>
-            <div className={style.innerWrapper}>
-                <input 
-                    type={showPassword ? "text" : type} 
-                    id={name}
-                    value={value} 
-                    name={name} 
-                    onChange={onChange}
-                    className={(!(name==="password")) ? style.inputTextField : style.inputTextFieldPassword}
-                    placeholder={label} 
-
-                />
-                {type === "password" && (
-                    <div className={style.showPasswordBtn} onClick={toggleShowPassword}>
-                        {(showPassword) ? 
-                        <img src={eyeVisiblePassword}alt="eyeVisiblePassword" className={style.showPasswordBtn_img}></img> :
-                        <img src={eyeHiddenPassword}alt="eyeHiddenPassword" className={style.showPasswordBtn_img}></img>}
-                    </div>)
-                }
-            </div>
-            {error && <div className={style.error}>{error}</div>}
-        </>
-    );
+const TextField = ({ label, id, type, name, error, register }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  //   console.log(register);
+  const toggleShowPassword = (e) => {
+    e.preventDefault();
+    setShowPassword((prevState) => !prevState);
+    let findPassword = document.getElementById("password");
+    findPassword.type = !showPassword ? "text" : "password";
+    // console.log(findPassword.type);
+    // console.log(showPassword);
+  };
+  console.log(error);
+  return (
+    <>
+      <label htmlFor={name} className={style.labelTextField}>
+        {" "}
+        {label}{" "}
+      </label>
+      <div className={style.innerWrapper}>
+        <input
+          {...register}
+          type={showPassword ? "text" : type}
+          id={id}
+          className={
+            !(name === "password")
+              ? style.inputTextField
+              : style.inputTextFieldPassword
+          }
+          placeholder={label}
+        />
+        {name === "password" && (
+          <div className={style.showPasswordBtn} onClick={toggleShowPassword}>
+            {showPassword ? (
+              <img
+                src={eyeVisiblePassword}
+                alt="eyeVisiblePassword"
+                className={style.showPasswordBtn_img}
+              ></img>
+            ) : (
+              <img
+                src={eyeHiddenPassword}
+                alt="eyeHiddenPassword"
+                className={style.showPasswordBtn_img}
+              ></img>
+            )}
+          </div>
+        )}
+      </div>
+      {error && <div className={style.error}>{error.message}</div>}
+    </>
+  );
 };
 
-TextField.defaultProps={
-    type: "text"
-};
-TextField.propTypes={
-    label: PropTypes.string,
-    type: PropTypes.string, 
-    name: PropTypes.string,
-    value: PropTypes.string, 
-    onChange: PropTypes.func,
-    error: PropTypes.string
+TextField.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  register: PropTypes.object,
+  error: PropTypes.object,
 };
 
 export default TextField;
--- src/app/components/ui/loginForm/loginForm.jsx
+++ src/app/components/ui/loginForm/loginForm.jsx
@@ -2,92 +2,120 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TextField from "../../common/textField";
 import Button from "../../common/buttons/button";
-import { validator } from "../../../utils/validator";
-import { showError } from "../../../utils/showError";
+// import { validator } from "../../../utils/validator";
+// import { showError } from "../../../utils/showError";
 import styleBtn from "../../common/buttons/buttons.module.css";
 import { loginService } from "../../../store/reducers/loginSlice";
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
-  const [data, setData] = useState({ email: "", password: "" });
-  const [errors, setErrors] = useState({});
-  const [isDirty, setIsDirty] = useState(false);
+  const {
+    register,
+    formState: { isValid, errors },
+    handleSubmit,
+  } = useForm({ mode: "onChange" });
+
+  // const [data, setData] = useState({ email: "", password: "" });
+  // const [errors, setErrors] = useState({});
+  // const [isDirty, setIsDirty] = useState(false);
   const dispatch = useDispatch();
 
-  const handleBlur = ({ target }) => {
-    const { name } = target;
-    setIsDirty(showError(data, name));
-  };
+  // const handleBlur = ({ target }) => {
+  //   const { name } = target;
+  //   setIsDirty(showError(data, name));
+  // };
 
-  const handleChange = ({ target }) => {
-    setData((prevState) => ({
-      ...prevState,
-      [target.name]: target.value,
-    }));
-  };
+  // const handleChange = ({ target }) => {
+  //   setData((prevState) => ({
+  //     ...prevState,
+  //     [target.name]: target.value,
+  //   }));
+  // };
 
-  const validatorConfig = {
-    email: {
-      isRequired: { message: "Email is required!" },
-      isEmail: { message: "Email is incorrect!" },
-    },
-    password: {
-      isRequired: { message: "Password is required!" },
-      isCapitalSymbol: { message: "Password must contain a capital letter!" },
-      isDigitSymbol: { message: "Password must contain a digit!" },
-      min: {
-        message: "Password must contain at least 8 characters!",
-        value: 8,
-      },
-    },
-  };
-  useEffect(() => {
-    validate();
-  }, [data]);
+  // const validatorConfig = {
+  //   email: {
+  //     isRequired: { message: "Email is required!" },
+  //     isEmail: { message: "Email is incorrect!" },
+  //   },
+  //   password: {
+  //     isRequired: { message: "Password is required!" },
+  //     isCapitalSymbol: { message: "Password must contain a capital letter!" },
+  //     isDigitSymbol: { message: "Password must contain a digit!" },
+  //     min: {
+  //       message: "Password must contain at least 8 characters!",
+  //       value: 8,
+  //     },
+  //   },
+  // };
+  // useEffect(() => {
+  //   validate();
+  // }, [data]);
 
-  const validate = () => {
-    const errors = validator(data, validatorConfig);
-    setErrors(errors);
-    return Object.keys(errors).length === 0;
-  };
+  // const validate = () => {
+  //   const errors = validator(data, validatorConfig);
+  //   // setErrors(errors);
+  //   return Object.keys(errors).length === 0;
+  // };
 
-  const isValid = Object.keys(errors).length === 0;
+  // const isValid = Object.keys(errors).length === 0;
 
-  const handSubmit = (e) => {
-    e.preventDefault();
-    const isValid = validate();
+  const onSubmit = (data) => {
+    // const isValid = validate();
     if (!isValid) return;
-    //console.log(data);
+
+    // console.log(data);
     dispatch(loginService(data));
   };
 
   const resServer = useSelector((state) => state.login.resMessage);
   return (
-    <form action="" onSubmit={handSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
+        register={register("email", {
+          required: "Email is required!",
+          pattern: {
+            value: /^\S+@\S+\.\S+$/g,
+            message: "Email is incorrect!",
+          },
+        })}
         label="Email"
         name="email"
-        value={data.email}
-        showError={isDirty}
-        onBlur={handleBlur}
-        onChange={handleChange}
+        // value={data.email}
+        // showError={isDirty}
+        // onBlur={handleBlur}
+        // onChange={handleChange}
         error={errors.email}
       />
       <TextField
+        register={register("password", {
+          required: "Password is required!",
+          minLength: {
+            value: 8,
+            message: "Password must contain at least 8 characters!",
+          },
+          validate: {
+            capitalSymbol: (s) => /[A-Z]+/g.test(s),
+            digitSymbol: (s) => /\d+/g.test(s),
+          },
+        })}
         label="Password"
         type="password"
+        id="password"
         name="password"
-        value={data.password}
-        showError={isDirty}
-        onBlur={handleBlur}
-        onChange={handleChange}
+        // value={data.password}
+        // showError={isDirty}
+        // onBlur={handleBlur}
+        // onChange={handleChange}
         error={errors.password}
       />
       <div>{resServer}</div>
       <Button
         value="Log in my WB account"
-        className={ (!isValid) ? 
-          `${styleBtn.commonButton } ${styleBtn.logInBtnInactive}` : 
-          `${styleBtn.commonButton } ${styleBtn.logInBtnActive}`}
+        className={
+          !isValid
+            ? `${styleBtn.commonButton} ${styleBtn.logInBtnInactive}`
+            : `${styleBtn.commonButton} ${styleBtn.logInBtnActive}`
+        }
         disabled={!isValid}
       />
     </form>
