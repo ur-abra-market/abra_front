be74ee1a752b48a7a5dd0003d51bc37e776a5ac3
Violetta <viol.fedoseenko@yandex.ru>
2022-07-15 22:18:28 +0300

    correct validation bug

--- src/app/components/ui/registerForm/registerForm.jsx
+++ src/app/components/ui/registerForm/registerForm.jsx
@@ -1,31 +1,24 @@
 import React, { useState } from "react";
 import TextField from "../../common/textField";
-// import RadioField from "../../common/radioField";
 import Button from "../../common/buttons/button";
-// import { validator } from "../../../utils/validator";
-// import { showError } from "../../../utils/showError";
 import style from "./registerForm.module.css"
 import styleBtn from "../../common/buttons/buttons.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { registerService } from "../../../store/reducers/registerSlice";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import PasswordComplexity from "../../common/passwordComplexity/passwordComplexity";
 
 const RegisterForm = () => {
   const [userStatus, setUserStatus] = useState("buyer");
-  // const [pathwordState, setPathwordState] = useState({
-  //   minLength: false,
-  //   digitSymbol: false,
-  //   capitalSymbol: false,
-  //   containsSymbols: false
-  // })
   const dispatch = useDispatch();
   const {
     register,
-    getValues,
+    watch,
     formState: { isValid, errors },
     handleSubmit,
   } = useForm({ mode: "onChange" });
+
+  const watchPasword = watch("password");
 
   const toggleUserStatus = () => {
     setUserStatus((prevState) =>
@@ -33,67 +26,8 @@ const RegisterForm = () => {
     );
 };
 
-  // const [data, setData] = useState({
-  //   first_name: "",
-  //   last_name: "",
-  //   email: "",
-  //   phone: "",
-  //   password: "",
-  //   additional_info: "",
-  //   status: "",
-  // });
-  // const [errors, setErrors] = useState({});
-  // const [isDirty, setIsDirty] = useState(false);
-
-  // const handleBlur = ({ target }) => {
-  //   const { name } = target;
-  //   setIsDirty(showError(data, name));
-  // };
-  // const handleChange = ({ target }) => {
-  //   setData((prevState) => ({
-  //     ...prevState,
-  //     [target.name]: target.value,
-  //   }));
-  // };
-
-  // const validatorConfig = {
-  //   first_name: { isRequired: { message: "First name is required!" } },
-  //   last_name: { isRequired: { message: "Second name is required!" } },
-  //   phone: {
-  //     isRequired: { message: "Phone number is required!" },
-  //     isPhoneNumber: { message: "Enter the correct number!" },
-  //   },
-  //   email: {
-  //     isRequired: { message: "Email is required!" },
-  //     isEmail: { message: "Email is incorrect!" },
-  //   },
-  //   password: {
-  //     isRequired: { message: "Password is required!" },
-  //     isCapitalSymbol: { message: "Password must contain a capital letter!" },
-  //     isDigitSymbol: { message: "Password must contain a digit!" },
-  //     min: {
-  //       message: "Password must contain at least 8 characters!",
-  //       value: 8,
-  //     },
-  //   },
-  // };
-  // useEffect(() => {
-  //   validate();
-  // }, [data]);
-
-  // const validate = () => {
-  //   const errors = validator(data, validatorConfig);
-  //   setErrors(errors);
-  //   return Object.keys(errors).length === 0;
-  // };
-
-  // const isValid = Object.keys(errors).length === 0;
-
   const onSubmit = (data) => {
-    // e.preventDefault();
-    // const isValid = validate();
     if (!isValid) return;
-    //console.log(data);
     dispatch(registerService(data));
     console.log(data);
   };
@@ -125,24 +59,6 @@ const RegisterForm = () => {
     </div>
 
     <form action="" onSubmit={handleSubmit(onSubmit)}>
-      {/* <TextField
-        label="First name"
-        name="first_name"
-        value={data.first_name}
-        showError={isDirty}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        error={errors.first_name}
-      />
-      <TextField
-        label="Second name"
-        name="last_name"
-        value={data.last_name}
-        showError={isDirty}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        error={errors.last_name}
-      /> */}
       <TextField
         register={register("email", {
           required: "Email is required!",
@@ -154,21 +70,7 @@ const RegisterForm = () => {
         label="Email"
         name="email"
         error={errors.email}
-        // value={data.email}
-        // showError={isDirty}
-        // onBlur={handleBlur}
-        // onChange={handleChange}
       />
-      {/* <TextField
-        label="Phone number"
-        name="phone"
-        type="tel"
-        value={data.phone}
-        showError={isDirty}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        error={errors.phone}
-      /> */}
       <TextField
         register={register("password", 
         {
@@ -189,39 +91,8 @@ const RegisterForm = () => {
         id="password"
         name="password"
         // error={errors.password}
-        
-        // value={data.password}
-        // showError={isDirty}
-        // onBlur={handleBlur}
-        // onChange={handleChange}
-        // error={errors.password}
       />
-      {/* <RadioField
-        options={[
-          { name: "Seller", value: "sellers" },
-          { name: "Supplier", value: "suppliers" },
-        ]}
-        value={data.status}
-        name="status"
-        onChange={handleChange}
-      /> */}
-        <PasswordComplexity valueOfNewPassword={getValues().password}/>
-          {/* <div className={`${style.requirement} ${style.capitalLetter}`}>
-              <div className={errors.type.capitalSymbol ? `${style.requirementCheckmark} ${style.done}` : style.requirementCheckmark}></div>
-              <div>1 capital letter</div>
-          </div>
-          <div className={style.requirement}>
-              <div className={errors.type.containsNumbers ? `${style.requirementCheckmark} ${style.done}` : style.requirementCheckmark}></div>
-              <div>1 number</div>
-          </div>
-          <div className={style.requirement}>
-              <div className={errors.type.passwordLength ? `${style.requirementCheckmark} ${style.done}` : style.requirementCheckmark}></div>
-              <div>8 symbols</div>
-          </div>
-          <div className={style.requirement}>
-              <div className={errors.type.containsSymbols ? `${style.requirementCheckmark} ${style.done}` : style.requirementCheckmark}></div>
-              <div>!/#/+/*</div>
-          </div> */}
+      <PasswordComplexity valueOfNewPassword={watchPasword}/>
       <div>{resServer}</div>
       <Button
         value="Sign up"
