851f7492f3202d84658f75272daddef51a3c1c11
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-07-18 21:25:01 +0300

    feat: move Feedback

--- src/app/components/pages/MainPage/MainPage.jsx
+++ src/app/components/pages/MainPage/MainPage.jsx
@@ -3,19 +3,24 @@ import './MainPage.css';
 import Slider from '../../common/Slider/Slider';
 import Sort from '../../common/Sort/Sort';
 import InfoBtn from '../../common/InfoBtn/InfoBtn';
+import Feedback from '../../common/Feedback/Feedback';
 
 const MainPage = () => {
   const dataArr = ['All categories', 'Clothes and accessories', 'Household products', 'Cosmetics and self care'];
   return (
-    <div className='main-page'>
-      <Sort />
-      <div className='Main__sliders'>
-        {dataArr.map((data, index) => (
-          <Slider key={`name-1-${index}`} title={data} />
-        ))}         
-      </div> 
-      <InfoBtn />         
-    </div>
+    <>
+      <div className='main-page'>
+        <Sort />
+        <div className='Main__sliders'>
+          {dataArr.map((data, index) => (
+            <Slider key={`name-1-${index}`} title={data} />
+          ))}         
+        </div> 
+        <InfoBtn />         
+      </div>
+      <Feedback />
+    </>
+    
   )
 }
 
--- src/app/layouts/main/Main.jsx
+++ src/app/layouts/main/Main.jsx
@@ -1,15 +1,13 @@
 import Header from "../../components/common/Header/Header";
 import MainPage from "../../components/pages/MainPage/MainPage";
 import Footer from "../../components/common/Footer/Footer";
-import Feedback from '../../components/common/Feedback/Feedback';
 import './Main.css'
 
 const Main = () => {
   return (
     <div className="container">
     <Header />
-    <MainPage />      
-    <Feedback />
+    <MainPage /> 
     <Footer />
     </div>        
   );
