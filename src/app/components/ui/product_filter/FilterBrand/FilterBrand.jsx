549583dcd50f4fb67be82c35f768aaeb2487bc6c
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-08-09 01:34:05 +0300

    fix: product filters

--- src/app/components/ui/product_filter/FilterBrand/FilterBrand.jsx
+++ src/app/components/ui/product_filter/FilterBrand/FilterBrand.jsx
@@ -6,10 +6,12 @@ const FilterBrand = () => {
   const brandList = ['Mavi', 'Kotton', 'LC Waikiki', 'Colin’s', 'DeFacto', 'Ipekyol']
   const brandCheck = brandList.map((_) => false);
   const [check, setCheck] = useState(brandCheck);
+  const [len, setLen] = useState(brandCheck.length);
   
 
   const changeState = (ctx) => {
-    const arrCheck = check.map((e, i) => brandList[i] === ctx ? !e : e)    
+    const arrCheck = check.map((e, i) => brandList[i] === ctx ? !e : e)   
+    setLen(arrCheck.filter(e => !e).length);
     setCheck(arrCheck);   
   }
 
@@ -17,24 +19,26 @@ const FilterBrand = () => {
     <div className='FilterBrand'>
       <h4 className='FilterBrand__title'>Brand</h4>
       <SearchFilter />
-      <div className='FilterBrand__list'>
-        {brandList.filter((b, i) => check[i]).map((b) => (
-          <div 
-            className='FilterBrand__list_item filter-item_active'
-            style={{background: '#000000', color: '#ffffff'}}
-            onClick={() => changeState(b)}
-            key={`brand_${b}`}>{b}</div>
-        ))}               
-      </div>
-      <div className='FilterBrand__list'>
-        {brandList.filter((b, i) => !check[i]).map((b) => (
-          <div 
-            className='FilterBrand__list_item'
-            style={{background: '#e5e5e5', color: '#000000'}}
-            onClick={() => changeState(b)}
-            key={`brand_${b}`}>{b}</div>
-        ))}            
-      </div>
+      <div className='FilterBrand__btns' style={{gap: len < check.length ? '24px' : '0px'}}>
+        <div className='FilterBrand__list'>
+          {brandList.filter((b, i) => check[i]).map((b) => (
+            <div 
+              className='FilterBrand__list_item filter-item_active'
+              style={{background: '#000000', color: '#ffffff'}}
+              onClick={() => changeState(b)}
+              key={`brand_${b}`}>{b}</div>
+          ))}               
+        </div>
+        <div className={len ? 'FilterBrand__list' : 'none'}>
+          {brandList.filter((b, i) => !check[i]).map((b) => (
+            <div 
+              className='FilterBrand__list_item'
+              style={{background: '#e5e5e5', color: '#000000'}}
+              onClick={() => changeState(b)}
+              key={`brand_${b}`}>{b}</div>
+          ))}            
+        </div>
+      </div>      
     </div>
   )
 }
