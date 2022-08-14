8b9428330b04ac7a40df09504b0716d30a2f7c84
AlexForw <lmityushkin@mail.ru>
2022-08-14 18:26:29 +0300

    feat: add redirect logic

--- src/app/components/ui/registerForm/registerForm.jsx
+++ src/app/components/ui/registerForm/registerForm.jsx
@@ -8,9 +8,14 @@ import { registerService } from "../../../store/reducers/registerSlice";
 import { useForm } from "react-hook-form";
 import PasswordComplexity from "../../common/passwordComplexity/passwordComplexity";
 import Form from "../../common/form/form";
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react";
 
 const RegisterForm = () => {
   const [userStatus, setUserStatus] = useState("suppliers");
+
+  const navigate = useNavigate()
+  
   const dispatch = useDispatch();
   const {
     register,
@@ -20,84 +25,94 @@ const RegisterForm = () => {
   } = useForm({ mode: "onChange" });
 
   const watchPasword = watch("password");
-
+  
   const toggleUserStatus = () => {
     setUserStatus((prevState) =>
-        prevState === "suppliers" ? "sellers" : "suppliers"
+      prevState === "suppliers" ? "sellers" : "suppliers"
     )
   };
-
+  
   const onSubmit = (data) => {
     if (!isValid) return;
-    dispatch(registerService(data));
+    dispatch(registerService({ ...data, userStatus }));
+    // goConfirmPage()
   };
-
+  
   const resServer = useSelector((state) => state.register.resMessage);
+  
+  useEffect(()=>{
+    
+    const goConfirmPage = () => navigate('/confirmEmail', { replace: true })
+
+    resServer === 'REGISTRATION_SUCCESSFUL' ?
+    goConfirmPage() :
+    console.log(2)
+  },[resServer, navigate])
 
   return (
     <>
-    <div className={style.buySellBtnWrappeer}>
-      <div className={style.flexContainer}>
-        <Button 
-          value="I'm here to buy"
-          className={userStatus === "suppliers"
-            ? styleBtn.userStatusBtnInactive
-            : styleBtn.userStatusBtnActive}
-          onClick={toggleUserStatus}
-        />
-        <Button 
-        value="I'm here to sell"
-        className={userStatus === "sellers"
-        ? styleBtn.userStatusBtnInactive
-        : styleBtn.userStatusBtnActive}
-        onClick={toggleUserStatus}
-        />
+      <div className={style.buySellBtnWrappeer}>
+        <div className={style.flexContainer}>
+          <Button
+            value="I'm here to buy"
+            className={userStatus === "suppliers"
+              ? styleBtn.userStatusBtnInactive
+              : styleBtn.userStatusBtnActive}
+            onClick={toggleUserStatus}
+          />
+          <Button
+            value="I'm here to sell"
+            className={userStatus === "sellers"
+              ? styleBtn.userStatusBtnInactive
+              : styleBtn.userStatusBtnActive}
+            onClick={toggleUserStatus}
+          />
+        </div>
       </div>
-    </div>
 
-    <Form action="" onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        register={register("email", {
-          required: "Email is required!",
-          pattern: {
-            value: /^\S+@\S+\.\S+$/g,
-            message: "Email is incorrect!",
-          },
-        })}
-        label="Email"
-        name="email"
-        error={errors.email}
-      />
-      <TextField
-        register={register("password", 
-        {
-          required: "Password is required!",
-          minLength: {
-            value: 8,
-            message: "Password must contain at least 8 characters!",
-          },
-          validate: {
-            capitalSymbol: (s) => /[A-Z]+/g.test(s),
-            digitSymbol: (s) => /\d+/g.test(s),
-            specialSymbol: (s) => /[!#+*]/g.test(s),
-          },
-        }
-        )}
-        label="Password"
-        type="password"
-        id="password"
-        name="password"
-      />
-      <PasswordComplexity valueOfNewPassword={watchPasword}/>
-      <div>{resServer}</div>
-      <Button
-        value="Sign up"
-        className={ (!isValid) ? 
-          `${styleBtn.commonButton } ${styleBtn.logInBtnInactive}` : 
-          `${styleBtn.commonButton } ${styleBtn.logInBtnActive}`}
-        disabled={!isValid}
-      />
-    </Form>
+      <Form action="" onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          register={register("email", {
+            required: "Email is required!",
+            pattern: {
+              value: /^\S+@\S+\.\S+$/g,
+              message: "Email is incorrect!",
+            },
+          })}
+          label="Email"
+          name="email"
+          error={errors.email}
+        />
+        <TextField
+          register={register("password",
+            {
+              required: "Password is required!",
+              minLength: {
+                value: 8,
+                message: "Password must contain at least 8 characters!",
+              },
+              validate: {
+                capitalSymbol: (s) => /[A-Z]+/g.test(s),
+                digitSymbol: (s) => /\d+/g.test(s),
+                specialSymbol: (s) => /[!#+*]/g.test(s),
+              },
+            }
+          )}
+          label="Password"
+          type="password"
+          id="password"
+          name="password"
+        />
+        <PasswordComplexity valueOfNewPassword={watchPasword} />
+        <div>{resServer}</div>
+        <Button
+          value="Sign up"
+          className={(!isValid) ?
+            `${styleBtn.commonButton} ${styleBtn.logInBtnInactive}` :
+            `${styleBtn.commonButton} ${styleBtn.logInBtnActive}`}
+          disabled={!isValid}
+        />
+      </Form>
     </>
   );
 };
