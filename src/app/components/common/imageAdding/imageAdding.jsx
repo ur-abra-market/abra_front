3088b264643efa0c1b3c95eded458453a4a247fe
AlexForw <lmityushkin@mail.ru>
2022-08-31 20:42:44 +0300

    fix: add check if item empty

--- src/app/components/common/imageAdding/imageAdding.jsx
+++ src/app/components/common/imageAdding/imageAdding.jsx
@@ -3,24 +3,26 @@ import iconImg from "../../../assets/img/icons/icon-img.png";
 import style from "./imageAdding.module.css";
 
 
-const ImageAdding = ({ label, placeholder, name }) => {
+const ImageAdding = ({ label, placeholder, error, register }) => {
     const [logoUrl, setLogoUrl] = useState('')
 
     const imgChange = (item) => {
         const reader = new FileReader()
 
         reader.onload = function () {
-            setLogoUrl(reader.result)
+            setLogoUrl(reader?.result)
         };
-        reader.readAsDataURL(item.target.files[0]);
+        if (item.target.files[0]) {
+            reader.readAsDataURL(item.target.files[0]);
+        }
     }
-
     return (
         <div className={style.profileLogoWrapper}>
 
             <div className={style.inputContainer}>
 
                 <input type="file"
+                    {...register}
                     accept="image/*"
                     name='profileLogo'
                     id='profileLogo'
@@ -45,10 +47,13 @@ const ImageAdding = ({ label, placeholder, name }) => {
 
             <div className={style.labelContainer}>
                 <label htmlFor='profileLogo'
-                className={style.label}>{label}</label>
-                
+                    className={style.label}>{label}</label>
+
                 <p className={style.placeholder}>{placeholder}</p>
             </div>
+
+            {error && <p className={style.inputError}>&#9888; {error}</p>}
+
         </div>
     );
 };
