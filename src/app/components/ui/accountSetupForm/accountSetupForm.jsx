35c5f07e9c0ca56c53ceb95c2e8fd064641ebbde
AlexForw <lmityushkin@mail.ru>
2022-08-31 20:44:06 +0300

    feat: add Validation in accountSetupForm

--- src/app/components/ui/accountSetupForm/accountSetupForm.jsx
+++ src/app/components/ui/accountSetupForm/accountSetupForm.jsx
@@ -3,12 +3,22 @@ import SelectLabelAbove from "../../common/selectLabelAbove";
 import ButtonReg from "../../common/buttons/buttonReg";
 import Form from "../../common/form";
 import FormTitle from "../../common/formTitle";
+import { useForm } from "react-hook-form";
 import style from "./accountSetupForm.module.css";
 
 const AccountSetupForm = () => {
 
+    const {
+        register,
+        formState: { errors, isValid },
+        handleSubmit,
+        reset,
+    } = useForm({ mode: 'onChange' })
 
-    
+    const onSubmit = (data) => {
+        reset()
+    }
+
     return (
         <div className={style.formWrapper}>
             <div className={style.formContainer}>
@@ -17,34 +27,50 @@ const AccountSetupForm = () => {
                     text={'This information will not be published. The data will only be used to create your account'}
                 />
 
-                <Form action="">
+                <Form action="" onSubmit={handleSubmit(onSubmit)}>
 
                     <div className={style.addName}>
 
                         <TextFieldLabelAbove
+                            register={
+                                register('fname', {
+                                    required: 'Field is required'
+                                })}
+                            error={errors?.fname?.message}
                             title={'First name'}
                             name={'fname'}
                             type={'text'}
-                            placeholder={'John'}
-                            error={'Please add text'} />
+                            placeholder={'John'} />
+
                         <TextFieldLabelAbove
+                            register={
+                                register('lname', {
+                                    required: 'Field is required'
+                                })}
+                            error={errors?.lname?.message}
                             title={'Last name'}
                             name={'lname'}
                             type={'text'}
-                            placeholder={'Johnson'}
-                            error={'Please add text'} />
+                            placeholder={'Johnson'} />
 
                     </div>
 
                     <SelectLabelAbove
+                        register={
+                            register('country', {
+                                required: 'Field is required'
+                            })}
+                        error={errors?.country?.message}
+
                         name={'country'}
                         title={'Country of company registration'}
                         placeholder={'Select'}
                         options={['USA', 'Germany', 'Brazil', 'France']}
-                        error={'Please add text'} />
+                    />
 
                     <div className={style.phoneNumber}>
                         <SelectLabelAbove
+                            register={register('code')}
                             name={'countryCode'}
                             title={'Personal phone number'}
                             options={['+90', '+44', '+77', '+1']}
@@ -52,20 +78,36 @@ const AccountSetupForm = () => {
 
                         <div className={style.marginFix}>
                             <TextFieldLabelAbove
+                                register={
+                                    register('tel', {
+                                        required: 'Field is required',
+                                        pattern: {
+                                            value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                                            message: "Invalid phone number",
+                                        }
+                                    })}
+                                error={errors?.tel?.message}
                                 name={'tel'}
                                 type={'tel'}
-                                placeholder={'(XXX) XXX - XX - XX'}
-                                error={'Please add text'} />
+                                placeholder={'(XXX) XXX - XX - XX'} />
                         </div>
 
                     </div>
 
                     <TextFieldLabelAbove
+                        register={
+                            register('license', {
+                                required: 'Field is required',
+                                minLength: {
+                                    value: 9,
+                                    message: 'Min length is 9',
+                                }
+                            })}
+                        error={errors?.license?.message}
                         title={'License or entrepreneur number'}
                         name={'license'}
                         type={'number'}
-                        placeholder={'000 – 00 – 0000'}
-                        error={'Please add text'} />
+                        placeholder={'000 – 00 – 0000'} />
 
                     <p className={style.licenseReminder}>
                         Use the number of any document authorizing the sale
@@ -73,7 +115,7 @@ const AccountSetupForm = () => {
 
                     <ButtonReg type={'submit'}
                         value={'Continue'}
-                        isValid={true} />
+                        isValid={!isValid} />
                 </Form>
 
             </div>
