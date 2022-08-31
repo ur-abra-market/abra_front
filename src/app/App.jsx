7cce35802601d7687c2f828ee26589f0f6f40e72
AlexForw <lmityushkin@mail.ru>
2022-08-31 04:40:03 +0300

    feat: add Routes for accountSetup, businessProfile

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,6 +6,8 @@ import Main from "./layouts/main"
 import NavBar from "./components/ui/navBar";
 import ForgotPasswordPage from "./components/pages/forgotPasswordPage";
 import ConfirmEmail from "./components/pages/confirmEmail";
+import AccountSetupPage from "./components/pages/accountSetupPage";
+import BusinessProfilePage from "./components/pages/businessProfilePage";
 
 
 
@@ -17,6 +19,8 @@ function App() {
         <Route path="/" element={< Main />} />
         <Route path="login" element={<Auth />}>
         </Route>
+        <Route path="account-setup" element={<AccountSetupPage />} />
+        <Route path="business-profile" element={<BusinessProfilePage />} />
         <Route path="forgotPasswordPage" element={< ForgotPasswordPage />} />
         <Route path="register/email-confirmation" element={<ConfirmEmail />} />
         <Route path="help" element={<p> Help </p>} />
