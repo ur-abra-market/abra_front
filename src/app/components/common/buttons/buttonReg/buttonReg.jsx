0a600eb02a1959a65b1966211d7752fa3284408c
AlexForw <lmityushkin@mail.ru>
2022-08-31 20:44:51 +0300

    feat: add check isValid for button

--- src/app/components/common/buttons/buttonReg/buttonReg.jsx
+++ src/app/components/common/buttons/buttonReg/buttonReg.jsx
@@ -5,7 +5,8 @@ const ButtonReg = ({ type, value, isValid }) => {
     return (
         <div>
             <button type={type}
-                className={style.button} >
+                className={isValid ? style.buttonOff : style.buttonOn}
+                disabled={isValid} >
                 {value}
             </button>
         </div>
