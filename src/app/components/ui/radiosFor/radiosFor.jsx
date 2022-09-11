5ffecaaa5791529856a0ea5592b7bca3ffa47fa8
AlexForw <lmityushkin@mail.ru>
2022-09-11 03:00:36 +0300

    feat: add element with some styled radios

---
+++ src/app/components/ui/radiosFor/radiosFor.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import RadioStyledElem from "../../common/radioStyledElem";
+import style from "./radiosFor.module.css";
+
+
+const RadiosFor = ({ register, state, array, title, name }) => {
+
+    const [choice, setChoice] = useState(state)
+
+    return (
+        <div>
+            <p className={style.title}>{title}</p>
+            <div className={style.radioWrapper}>
+                {array.map((e) => {
+                    return <RadioStyledElem name={name}
+                        choice={choice}
+                        setChoice={setChoice}
+                        register={register}
+                        value={e}
+                        key={Math.random() + e} />
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default RadiosFor;
