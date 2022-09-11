97120ff5ca516ce3bf4d0fc3a218ae60b097af21
AlexForw <lmityushkin@mail.ru>
2022-09-11 03:04:52 +0300

    feat: add ProductListPage

---
+++ src/app/components/pages/productListRegistrationPage/index.js
@@ -0,0 +1,2 @@
+import ProductListRegistrationPage from "./productListRegistrationPage";
+export default ProductListRegistrationPage;
---
+++ src/app/components/pages/productListRegistrationPage/productListRegistrationPage.jsx
@@ -0,0 +1,11 @@
+import ProductListRegistrationForm from "../../ui/productListRegistrationForm";
+
+const ProductListRegistrationPage = () => {
+    return (
+        <div>
+           <ProductListRegistrationForm />
+        </div>
+    );
+};
+
+export default ProductListRegistrationPage;
