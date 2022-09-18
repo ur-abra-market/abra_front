04d73e203510424884e23be14aa2aed25d9808c7
AlexForw <lmityushkin@mail.ru>
2022-09-18 15:37:01 +0300

    fix: change key for option elem

--- src/app/components/common/selectLabelAbove/selectLabelAbove.jsx
+++ src/app/components/common/selectLabelAbove/selectLabelAbove.jsx
@@ -5,32 +5,28 @@ const SelectLabelAbove = ({ title, name, placeholder, options, error, register }
     return (
         <div>
             <p className={style.selectTitle}>{title}</p>
-
-            <div className={style.selectContainer}>
-                <select
-                    {...register}
-                    defaultValue={''}
-                    name={name}
-                    className={style.selectField}>
-
-                    {placeholder &&
-                        <option
-                            value='' disabled hidden>
-                            {placeholder}
-                        </option>
-                    }
-
-                    {options.map((elem) => {
-                        return (<option
-                            className={style.selectOption}
-                            key={Math.random()} value={elem}>
-                            {elem}
-                        </option>)
-                    })}
-                </select>
-                <span className={style.selectArrow}>&#9660;</span>
-
-            </div>
+                    <div className={style.selectContainer}>
+                        <select
+                            {...register}
+                            name={name}
+                            defaultValue={''}
+                            className={style.selectField}>
+                            {placeholder &&
+                                <option
+                                    value='' disabled hidden>
+                                    {placeholder}
+                                </option>
+                            }
+                            {options.map((elem) => {
+                                return (<option
+                                    className={style.selectOption}
+                                    key={elem} value={elem}>
+                                    {elem}
+                                </option>)
+                            })}
+                        </select>
+                        <span className={style.selectArrow}>&#9660;</span>
+                    </div>
 
 
             {error && <p className={style.selectError}>&#9888; {error}</p>}
