2ea2ca6b3111ace08d51c2d4bb25feb38200363a
fouinyla <mefouiny@gmail.com>
2022-09-19 16:44:33 +0300

    add .env; edit http.service

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const httpService = axios.create({
-    baseURL: "http://wbplt-env.eba-qxbp72mz.eu-central-1.elasticbeanstalk.com/",
+    baseURL: "http://" + process.env.REACT_APP_SERVER_URL + "/",
 });
 
 export default httpService;
