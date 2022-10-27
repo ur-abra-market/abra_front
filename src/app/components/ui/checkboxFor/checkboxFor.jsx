d4ed9708ace6647db992aa0795d46654faed1d5d
Beshil <ersanbievbesh@gmail.com>
2022-10-27 20:00:51 +0300

    fix error with checkbox

--- src/app/components/ui/checkboxFor/checkboxFor.jsx
+++ src/app/components/ui/checkboxFor/checkboxFor.jsx
@@ -1,3 +1,4 @@
+import { DevTool } from "@hookform/devtools";
 import { useCallback } from "react";
 import CheckboxStyledElem from "../../common/checkboxStyledElem";
 import style from "./checkboxFor.module.css";
@@ -7,9 +8,10 @@ const CheckboxFor = ({ register, array, title, getValues }) => {
     const values = getValues(array.map((v, index) => v));
 
     const isValid = values.some((v) => v);
-
+    console.log(isValid);
     return isValid;
   }, []);
+
   return (
     <div>
       <p className={style.title}>{title}</p>
