8929014cf366f3ac08d7d5261d52868d36ddf9aa
Beshil <ersanbievbesh@gmail.com>
2022-11-06 19:45:35 +0300

    update

--- src/app/components/common/buttons/BtnNewBest/BtnNewBest.jsx
+++ src/app/components/common/buttons/BtnNewBest/BtnNewBest.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './BtnNewBest.css'
 
 const BtnNewBest = ({ name }) => {
   return <div className="BtnNewBest">{name}</div>
 }
-
+BtnNewBest.propTypes={
+  name:PropTypes.string,
+}
 export default BtnNewBest
