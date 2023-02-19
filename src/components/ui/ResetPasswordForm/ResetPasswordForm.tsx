f816d68c9396a6336c3b62cf8c8888954036f49e
Nikita1025 <nekit1025l@gmail.com>
2023-02-19 21:19:39 +0300

    refactor reset password

--- src/components/ui/ResetPasswordForm/ResetPasswordForm.tsx
+++ src/components/ui/ResetPasswordForm/ResetPasswordForm.tsx
@@ -2,64 +2,54 @@ import React, { FC } from 'react';
 
 import { useForm } from 'react-hook-form';
 
-import { Button } from '../../buttons';
-import styleBtn from '../../buttons/Buttons.module.css';
 import Form from '../../Form';
 import PasswordComplexity from '../../new-components/PasswordComplexity';
-import TextField from '../../TextField';
 
 import style from './ResetPasswordForm.module.css';
+import * as yup from "yup";
+import { FormDataValuesType } from "../../../pages/AuthPage/AuthType";
+import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
+import { Button, Input } from "../../ui-kit";
 
 interface ResetPasswordFormProps {
   handleChangeModalActive: any;
 }
+const schema = yup
+  .object({
+    password: yup.string().matches(
+      /^.*(?=.{8,})((?=.*[!#+*]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/
+    )
+  })
+  .required();
 const ResetPasswordForm: FC<ResetPasswordFormProps> = ({ handleChangeModalActive }) => {
   const {
     register,
     watch,
     formState: { isValid },
     handleSubmit,
-  } = useForm({ mode: 'onChange' });
+  } =  useForm<FormDataValuesType>({
+    resolver: yupResolver(schema),
+    mode: 'all',
+  });
   const watchPasword = watch('password');
   const onSubmit = (): void => {
     // eslint-disable-next-line no-useless-return
     if (!isValid) return;
   };
-  const textFieldClasses = {
-    label: `${style.textFieldLabel}`,
-    inputWrapper: `${style.inputWrapper}`,
-    input: `${style.textFieldInput}`,
-  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)} className={style.resetPasswordForm}>
-      <TextField
-        register={register('password', {
-          required: 'Password is required!',
-          minLength: {
-            value: 8,
-            message: 'Password must contain at least 8 characters!',
-          },
-          validate: {
-            capitalSymbol: s => /[A-Z]+/g.test(s),
-            digitSymbol: s => /\d+/g.test(s),
-            specialSymbol: s => /[!#+*]/g.test(s),
-          },
-        })}
-        label="Password"
+      <Input
+        {...register('password')}
+        classNameWrapper={style.input_wrapper}
+        placeholder="Password"
         type="password"
-        id="password"
-        name="password"
-        classes={textFieldClasses}
+        variant="password"
       />
       <PasswordComplexity valueOfNewPassword={watchPasword} />
       <Button
-        value="Save"
-        className={
-          !isValid
-            ? `${styleBtn.commonButton} ${styleBtn.logInBtnInactive}`
-            : `${styleBtn.commonButton} ${styleBtn.logInBtnActive}`
-        }
+        label="Save"
+        className={style.button_save}
         disabled={!isValid}
         onClick={handleChangeModalActive}
       />
