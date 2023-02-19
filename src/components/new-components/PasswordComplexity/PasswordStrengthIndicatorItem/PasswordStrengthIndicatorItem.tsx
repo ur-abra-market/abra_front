f2a4b9b9c272f3e68c55090b1408e331b6807d74
Alexander Kuznetsov <gl633bl@gmail.com>
2023-02-19 17:24:47 +0300

    Refactor: styles

--- src/components/new-components/PasswordComplexity/PasswordStrengthIndicatorItem/PasswordStrengthIndicatorItem.tsx
+++ src/components/new-components/PasswordComplexity/PasswordStrengthIndicatorItem/PasswordStrengthIndicatorItem.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from 'react';
 
+import cn from 'classnames';
+
 import style from './PasswordStrengthIndicatorItem.module.css';
 
 interface PasswordStrengthIndicatorItemProps {
-  isValid: boolean;
+  isValid?: boolean;
   text: string;
 }
 const PasswordStrengthIndicatorItem: FC<PasswordStrengthIndicatorItemProps> = ({
-  isValid,
+  isValid = false,
   text,
 }): JSX.Element => {
   return (
     <div className={style.requirement}>
-      <div className={isValid ? style.requirementMet : style.requirementNotMet} />
+      <div
+        className={cn({
+          [style.requirement_met]: isValid,
+          [style.requirement_not_met]: !isValid,
+        })}
+      />
       <div>{text}</div>
     </div>
   );
--- src/layouts/Auth/AuthType.ts
+++
@@ -1,4 +0,0 @@
-export type FormDataValuesType ={
-  email:string
-  password:string
-}
