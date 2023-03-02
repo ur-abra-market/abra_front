884a57fee636a95d207c0df2bc2556de2dd55158
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-03-02 20:05:14 +0300

    feat: add test data

--- src/pages/SellerFavoritesList/SellerFavoritesList.tsx
+++ src/pages/SellerFavoritesList/SellerFavoritesList.tsx
@@ -12,6 +12,22 @@ import style from './SellerFavoritesList.module.css';
 const SellerFavoritesList = (): JSX.Element => {
   const arr = [];
 
+  // const product: IShortCardProduct = {
+  //   id: +new Date(),
+  //   name: 'trousers',
+  //   description: 'trousers description',
+  //   total_orders: 44,
+  //   grade_average: '4.5',
+  //   date_added: '2021-04-11T10:20:30Z',
+  //   with_discount: 2,
+  //   price_include_discount: '50',
+  //   min_quantity: 5,
+  //   value_price: '100',
+  //   is_favorite: 1,
+  //   image_url:
+  //     'https://images.asos-media.com/products/asos-design-cargo-tapered-trousers-in-black-with-toggles/202796442-1-black?$n_750w$&wid=750&hei=750&fit=crop',
+  // };
+
   const exampleCount = 24;
 
   for (let i = 0; i < exampleCount; i += 1) {
@@ -27,8 +43,6 @@ const SellerFavoritesList = (): JSX.Element => {
           <Search className={style.search} placeholder="Search within my favorites" />
         </div>
         <div className={style.main}>
-          {/* <ProductCard /> */}
-
           {arr.map((item, index) => (
             <div className={style.product_card} key={index}>
               {item}
