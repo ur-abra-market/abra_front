b1ab35d39c7f0007fd7cb6a989bd83d1b7c799b5
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 13:34:28 +0300

    fix stylelint ProductListPage

--- src/pages/ProductListPage/ProductListPage.tsx
+++ src/pages/ProductListPage/ProductListPage.tsx
@@ -1,34 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 
-import style from './ProductListPage.module.css';
+import style from './ProductListPage.module.css'
 
-import ProductFilter from 'components/ui/product/ProductFilter';
-import ProductList from 'components/ui/product/ProductList';
-import Footer from 'layouts/Footer';
-import Header from 'layouts/Header';
-import { useAppDispatch, useAppSelector } from 'store/hooks';
-import { productPaginateService } from 'store/reducers/productPaginateSlice';
+import ProductFilter from 'components/ui/product/ProductFilter'
+import ProductList from 'components/ui/product/ProductList'
+import Footer from 'layouts/Footer'
+import Header from 'layouts/Header'
+import { useAppDispatch, useAppSelector } from 'store/hooks'
+import { productPaginateService } from 'store/reducers/productPaginateSlice'
 
 const ProductListPage = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const paginate = useAppSelector(state => state.paginate);
-  const filter = useAppSelector(state => state.filter);
-  const data = { ...filter, ...paginate };
+  const dispatch = useAppDispatch()
+  const paginate = useAppSelector((state) => state.paginate)
+  const filter = useAppSelector((state) => state.filter)
+  const data = { ...filter, ...paginate }
 
   useEffect(() => {
-    dispatch(productPaginateService(data));
-  });
+    dispatch(productPaginateService(data))
+  })
 
   return (
     <>
       <Header />
-      <div className={style.productListPage}>
+      <div className={style.product_list_page}>
         <ProductFilter />
         <ProductList />
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default ProductListPage;
+export default ProductListPage
