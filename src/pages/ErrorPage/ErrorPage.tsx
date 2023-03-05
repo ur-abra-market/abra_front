ac080d157403fb2d2377b1336af63c5ae3773cfd
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 13:22:50 +0300

    fix stylelint ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { useRouteError } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom'
 
-import styles from './ErrorPage.module.css';
+import styles from './ErrorPage.module.css'
 
 export const ErrorPage = (): JSX.Element => {
-  const error = useRouteError();
-
-  console.error(error);
+  const error = useRouteError()
 
   return (
-    <div id="error-page" className={styles.errorPage}>
+    <div id="error-page" className={styles.error_page}>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
@@ -18,7 +16,7 @@ export const ErrorPage = (): JSX.Element => {
         <i>{error?.statusText || error?.message}</i>
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default ErrorPage;
+export default ErrorPage
