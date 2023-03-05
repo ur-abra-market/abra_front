00a710b5a21ab77dbe62856e8a93996fad88f27e
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 16:05:52 +0300

    fix stylelint MaterialInputs

--- src/components/ui/MaterialInputs/MaterialInputs.tsx
+++ src/components/ui/MaterialInputs/MaterialInputs.tsx
@@ -1,19 +1,19 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState } from 'react'
 
-import RelatedInputs from '../../RelatedInputs';
+import RelatedInputs from '../../RelatedInputs'
 
-import style from './MaterialInputs.module.css';
+import style from './MaterialInputs.module.css'
 
-const fakeArr: number[] = [];
+const fakeArr: number[] = []
 
 interface MaterialInputsProps {
-  register: any;
-  mainTitle: string;
-  optTitle: string;
-  mainPlaceholder: string;
-  optPlaceholder: string;
-  mainType: string;
-  optType: string;
+  register: any
+  mainTitle: string
+  optTitle: string
+  mainPlaceholder: string
+  optPlaceholder: string
+  mainType: string
+  optType: string
 }
 const MaterialInputs: FC<MaterialInputsProps> = ({
   register,
@@ -22,17 +22,17 @@ const MaterialInputs: FC<MaterialInputsProps> = ({
   mainPlaceholder,
   optPlaceholder,
   mainType,
-  optType,
-}) => {
-  const [count, setCount] = useState(1);
+  optType
+}): JSX.Element => {
+  const [count, setCount] = useState(1)
 
   const addInputs = (): void => {
-    setCount(count + 1);
-    fakeArr.push(count);
-  };
+    setCount(count + 1)
+    fakeArr.push(count)
+  }
 
   return (
-    <div className={style.inputsContainer}>
+    <div className={style.inputs_container}>
       <RelatedInputs
         register={register}
         mainTitle={mainTitle}
@@ -45,7 +45,7 @@ const MaterialInputs: FC<MaterialInputsProps> = ({
         optPlaceholder={optPlaceholder}
       />
 
-      {fakeArr.map(e => {
+      {fakeArr.map((e) => {
         return (
           <div key={e}>
             <RelatedInputs
@@ -58,14 +58,15 @@ const MaterialInputs: FC<MaterialInputsProps> = ({
               optPlaceholder={optPlaceholder}
             />
           </div>
-        );
+        )
       })}
 
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
       <p className={style.add} onClick={addInputs}>
         + Add material
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default MaterialInputs;
+export default MaterialInputs
