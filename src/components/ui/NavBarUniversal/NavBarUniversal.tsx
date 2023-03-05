75ef2debb2a92275296be3d6a0d6329c701ed2fd
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 16:07:12 +0300

    fix stylelint NavBarUniversal

--- src/components/ui/NavBarUniversal/NavBarUniversal.tsx
+++ src/components/ui/NavBarUniversal/NavBarUniversal.tsx
@@ -1,18 +1,21 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode } from 'react'
 
-import style from './NavBarUniversal.module.css';
+import style from './NavBarUniversal.module.css'
 
 interface NavBarUniversalProps {
-  logo: JSX.Element;
-  children: ReactNode;
+  logo: JSX.Element
+  children: ReactNode
 }
-const NavBarUniversal: FC<NavBarUniversalProps> = ({ logo, children }) => {
+const NavBarUniversal: FC<NavBarUniversalProps> = ({
+  logo,
+  children
+}): JSX.Element => {
   return (
-    <nav className={style.navBarWrapper}>
-      <h1 className={style.logoSection}>{logo}</h1>
-      <div className={style.linksSection}>{children}</div>
+    <nav className={style.nav_bar_wrapper}>
+      <h1 className={style.logo_section}>{logo}</h1>
+      <div className={style.links_section}>{children}</div>
     </nav>
-  );
-};
+  )
+}
 
-export default NavBarUniversal;
+export default NavBarUniversal
