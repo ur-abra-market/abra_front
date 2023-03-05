4ae933012db61d60897ec6b610fe1984ac1bd22f
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 17:31:09 +0300

    fix stylelint ProductPrice

--- src/components/ui/product/ProductPrice/ProductPrice.tsx
+++ src/components/ui/product/ProductPrice/ProductPrice.tsx
@@ -1,19 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
 
-import style from './ProductPrice.module.css';
+import style from './ProductPrice.module.css'
 
 interface ProductPriceProps {
-  price: string;
-  quantity: string;
+  price: string
+  quantity: string
 }
 
-const ProductPrice: FC<ProductPriceProps> = ({ price, quantity }) => {
+const ProductPrice: FC<ProductPriceProps> = ({
+  price,
+  quantity
+}): JSX.Element => {
   return (
-    <div className={style.productPrice}>
+    <div className={style.product_price}>
       <div className="amount">${price}/pc</div>
       <span>{`/from ${quantity} pcs`}</span>
     </div>
-  );
-};
+  )
+}
 
-export default ProductPrice;
+export default ProductPrice
