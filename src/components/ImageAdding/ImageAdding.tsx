e7f9fd3cacdc318d3cbff0d19e587a10581c3522
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 12:08:16 +0300

    fix stylelint ImageAdding

--- src/components/ImageAdding/ImageAdding.tsx
+++ src/components/ImageAdding/ImageAdding.tsx
@@ -1,18 +1,18 @@
-import React, { ChangeEvent, FC } from 'react';
+import React, { ChangeEvent, FC } from 'react'
 
-import iconImg from '../../assets/img/icons/icon-img.png';
+import iconImg from '../../assets/img/icons/icon-img.png'
 
-import style from './ImageAdding.module.css';
+import style from './ImageAdding.module.css'
 
 interface ImageAddingProps {
-  label?: string;
-  placeholder?: string;
-  error?: string;
-  imgUrl?: string;
-  setImgUrl?: any;
-  images?: any[];
-  setImages?: any;
-  register?: any;
+  label?: string
+  placeholder?: string
+  error?: string
+  imgUrl?: string
+  setImgUrl?: any
+  images?: any[]
+  setImages?: any
+  register?: any
 }
 const ImageAdding: FC<ImageAddingProps> = ({
   label,
@@ -22,34 +22,34 @@ const ImageAdding: FC<ImageAddingProps> = ({
   imgUrl,
   setImgUrl,
   images,
-  setImages,
-}) => {
+  setImages
+}): JSX.Element => {
   const imgChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const reader = new FileReader();
+    const reader = new FileReader()
 
     reader.onload = () => {
-      setImgUrl(reader?.result);
-    };
+      setImgUrl(reader?.result)
+    }
 
-    if (!e.target.files) return;
+    if (!e.target.files) return
 
     // TODO - заглущка проверка на images
     if (e.target.files[0] && images) {
-      reader.readAsDataURL(e.target.files[0]);
-      setImages([...images, e.target.files[0]]);
+      reader.readAsDataURL(e.target.files[0])
+      setImages([...images, e.target.files[0]])
     }
-  };
+  }
 
   return (
-    <div className={style.profileLogoWrapper}>
-      <div className={style.inputContainer}>
+    <div className={style.profile_logo_wrapper}>
+      <div className={style.input_container}>
         <input
           type="file"
           {...register}
           accept="image/*"
           name="file"
           id="profileLogo"
-          className={label ? style.inputFileLabel : style.inputFile}
+          className={label ? style.input_file_label : style.input_file}
           onChange={imgChange}
         />
 
@@ -58,25 +58,25 @@ const ImageAdding: FC<ImageAddingProps> = ({
             src={imgUrl}
             alt="avatar img"
             id="avatarImg"
-            className={label ? style.avatarImgLabel : style.avatarImg}
+            className={label ? style.avatar_img_label : style.avatar_img}
           />
         ) : (
           <img
             src={iconImg}
             alt="icon img"
             id="iconImg"
-            className={label ? style.iconImgLabel : style.iconImg}
+            className={label ? style.icon_img_label : style.icon_img}
           />
         )}
 
         {label ? (
-          <div className={style.iconBackgroundLabel} />
+          <div className={style.icon_background_label} />
         ) : (
-          <div className={style.iconBackground} />
+          <div className={style.icon_background} />
         )}
       </div>
 
-      <div className={style.labelContainer}>
+      <div className={style.label_container}>
         <label htmlFor="profileLogo" className={style.label}>
           {label}
         </label>
@@ -84,9 +84,9 @@ const ImageAdding: FC<ImageAddingProps> = ({
         <p className={style.placeholder}>{placeholder}</p>
       </div>
 
-      {error && <p className={style.inputError}>&#9888; {error}</p>}
+      {error && <p className={style.input_error}>&#9888; {error}</p>}
     </div>
-  );
-};
+  )
+}
 
-export default ImageAdding;
+export default ImageAdding
