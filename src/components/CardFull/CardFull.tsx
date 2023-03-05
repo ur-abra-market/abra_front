129e39bebde92f59a7b0e896dccc50338feda9a4
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 11:37:33 +0300

    fix stylelint CardFull

--- src/components/CardFull/CardFull.tsx
+++ src/components/CardFull/CardFull.tsx
@@ -1,63 +1,63 @@
-import React, { CSSProperties, FC, useState } from 'react';
+import React, { CSSProperties, FC, useState } from 'react'
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { changeById } from '../../store/reducers/basketSlice';
-import { active } from '../../store/reducers/productPaginateSlice';
-import { BtnNewBest } from '../buttons';
-import ImgSlider from '../ImgSlider';
-import ProductQuantityControl from '../ProductQuantityControl';
-import Stars from '../Stars';
-import ProductPath from '../ui/product/ProductPath';
-import ProductPrice from '../ui/product/ProductPrice';
-import SupplierCard from '../ui/product/SupplierCard';
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { changeById } from '../../store/reducers/basketSlice'
+import { active } from '../../store/reducers/productPaginateSlice'
+import { BtnNewBest } from '../buttons'
+import ImgSlider from '../ImgSlider'
+import ProductQuantityControl from '../ProductQuantityControl'
+import Stars from '../Stars'
+import ProductPath from '../ui/product/ProductPath'
+import ProductPrice from '../ui/product/ProductPrice'
+import SupplierCard from '../ui/product/SupplierCard'
 
-import style from './CardFull.module.css';
+import style from './CardFull.module.css'
 
 interface CardFullProps {
-  props: any;
-  product_id?: string;
+  props: any
+  product_id?: string
 }
 const CardFull: FC<CardFullProps> = ({ props }): JSX.Element => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const basket = useAppSelector(state => state.basket.basketProduct);
-  const product = basket.find(obj => obj.product_id === props.product_id);
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const basket = useAppSelector((state) => state.basket.basketProduct)
+  const product = basket.find((obj) => obj.product_id === props.product_id)
 
-  const [sum, setSum] = useState(0);
-  const propsNew = product || { ...props, ...{ sum } };
+  const [sum, setSum] = useState(0)
+  const propsNew = product || { ...props, ...{ sum } }
 
   const style1: CSSProperties = {
     flexDirection: 'row',
     alignItems: 'flex-end',
-    gap: '0px',
-  };
+    gap: '0px'
+  }
   const style2: CSSProperties = {
     flexDirection: 'column',
     alignItems: 'flex-start',
-    gap: '8px',
-  };
+    gap: '8px'
+  }
 
   const handlerBasket = (): void => {
-    setSum(100);
-    propsNew.sum = 100;
-    const newObj = propsNew;
+    setSum(100)
+    propsNew.sum = 100
+    const newObj = propsNew
 
-    dispatch(changeById({ newObj }));
-  };
+    dispatch(changeById({ newObj }))
+  }
 
   return (
-    <div className={style.cardFull}>
+    <div className={style.card_full}>
       <ImgSlider srcArr={propsNew.images} />
-      <div className={style.cardFull__info}>
-        <div className={style.cardFull__block1}>
-          <div className={style.cardFull__direction}>
+      <div className={style.card_full_info}>
+        <div className={style.card_full_block1}>
+          <div className={style.card_full_direction}>
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
             <h4
               onClick={() => {
-                navigate('../product');
-                dispatch(active(propsNew));
+                navigate('../product')
+                dispatch(active(propsNew))
               }}
             >
               {propsNew.info.name}
@@ -66,31 +66,31 @@ const CardFull: FC<CardFullProps> = ({ props }): JSX.Element => {
           </div>
           <ProductPath pathArr={[]} />
         </div>
-        <div className={style.cardFull__block2}>
+        <div className={style.card_full_block2}>
           <div
-            className={style.cardFull__price_stars}
+            className={style.card_full_price_stars}
             style={propsNew.sum === 0 ? style1 : style2}
           >
             <ProductPrice
               price={propsNew.info.value_price}
               quantity={propsNew.info.quantity}
             />
-            <div className={style.cardFull__stars_reviews}>
+            <div className={style.card_full_stars_reviews}>
               <Stars reward={+propsNew.info.grade_average} />
               <span>{`/${propsNew.info.total_reviews} reviews`}</span>
             </div>
           </div>
           <div
-            className={style.cardFull__orders}
+            className={style.card_full_orders}
           >{`${propsNew.info.total_orders} Orders`}</div>
         </div>
-        <div className={style.cardFull__block3}>
+        <div className={style.card_full_block3}>
           {propsNew.sum > 0 ? (
             <ProductQuantityControl obj={propsNew} />
           ) : (
             <div
               role="presentation"
-              className={style.cardFull__basket}
+              className={style.card_full_basket}
               onClick={() => handlerBasket()}
             >
               <div />
@@ -100,7 +100,7 @@ const CardFull: FC<CardFullProps> = ({ props }): JSX.Element => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CardFull;
+export default CardFull
