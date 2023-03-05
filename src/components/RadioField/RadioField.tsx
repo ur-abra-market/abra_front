1ec755249e4658354b9eb5dc7e2e436147842c8f
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 12:28:51 +0300

    fix stylelint RadioField

--- src/components/RadioField/RadioField.tsx
+++ src/components/RadioField/RadioField.tsx
@@ -1,25 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
 
-import PropTypes, { string } from 'prop-types';
-
-import style from './RadioField.module.css';
+import style from './RadioField.module.css'
 
 interface RadioFieldProps {
-  options: any[];
-  name: string;
-  onChange: any;
-  value: string;
+  options: any[]
+  name: string
+  onChange: any
+  value: string
 }
 
 const RadioField: FC<RadioFieldProps> = ({
   options,
   name,
   onChange,
-  value,
+  value
 }): JSX.Element => {
   return (
-    <div className={style.labelWrapper}>
-      {options.map(option => (
+    <div className={style.label_wrapper}>
+      {options.map((option) => (
         <div key={`${option.name}_${option.value}`}>
           <input
             type="radio"
@@ -29,11 +27,13 @@ const RadioField: FC<RadioFieldProps> = ({
             checked={option.value === value}
             onChange={onChange}
           />
-          <label htmlFor={`${option.name}_${option.value}`}>{option.name}</label>
+          <label htmlFor={`${option.name}_${option.value}`}>
+            {option.name}
+          </label>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default RadioField;
+export default RadioField
