92b493a37212c623d9b2c766364c5147fdc0b939
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 17:08:11 +0300

    fix stylelint FilterGrowth

--- src/components/ui/product_filter/FilterGrowth/FilterGrowth.tsx
+++ src/components/ui/product_filter/FilterGrowth/FilterGrowth.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import style from './FilterGrowth.module.css';
+import style from './FilterGrowth.module.css'
 
 const FilterGrowth = (): JSX.Element => {
   const growthList = [
@@ -14,28 +14,29 @@ const FilterGrowth = (): JSX.Element => {
     '146-152',
     '158-164',
     '170-176',
-    '182-188',
-  ];
-  const growthCheck = growthList.map(() => false);
+    '182-188'
+  ]
+  const growthCheck = growthList.map(() => false)
 
-  const [check, setCheck] = useState(growthCheck);
+  const [check, setCheck] = useState(growthCheck)
 
   const changeState = (index: number): void => {
-    const arrCheck = check.map((e, i) => (i === index ? !e : e));
+    const arrCheck = check.map((e, i) => (i === index ? !e : e))
 
-    setCheck(arrCheck);
-  };
+    setCheck(arrCheck)
+  }
 
   return (
-    <div className={style.filterGrowth}>
+    <div className={style.filter_growth}>
       <h4>Growth, cm</h4>
-      <div className={style.filterGrowth__list}>
+      <div className={style.filter_growth_list}>
         {growthList.map((s, i) => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
           <div
-            className={style.filterGrowth__list_item}
+            className={style.filter_growth_list_item}
             style={{
               background: check[i] ? '#000000' : '#ffffff',
-              color: check[i] ? '#ffffff' : '#000000',
+              color: check[i] ? '#ffffff' : '#000000'
             }}
             onClick={() => changeState(i)}
             key={`growth_${s}`}
@@ -45,7 +46,7 @@ const FilterGrowth = (): JSX.Element => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FilterGrowth;
+export default FilterGrowth
