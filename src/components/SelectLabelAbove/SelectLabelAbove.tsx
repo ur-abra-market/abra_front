f3d30cc691083960f30a4ffbac8935cbfac396cb
Nikita1025 <nekit1025l@gmail.com>
2023-03-05 12:51:58 +0300

    fix stylelint SelectLabelAbove

--- src/components/SelectLabelAbove/SelectLabelAbove.tsx
+++ src/components/SelectLabelAbove/SelectLabelAbove.tsx
@@ -1,18 +1,26 @@
-import React, { ChangeEvent, DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes
+} from 'react'
 
-import style from './SelectLabelAbove.module.css';
+import style from './SelectLabelAbove.module.css'
 
 interface SelectLabelAboveProps {
-  onChange?: Function;
-  onChangeOption?: Function;
-  title: string;
-  name?: string;
-  placeholder?: string;
-  options?: any[];
-  error?: string;
-  restProps?: any;
-  register?: any;
-  selectProps?: DetailedHTMLProps<HTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
+  onChange?: Function
+  onChangeOption?: Function
+  title: string
+  name?: string
+  placeholder?: string
+  options?: any[]
+  error?: string
+  restProps?: any
+  register?: any
+  selectProps?: DetailedHTMLProps<
+    HTMLAttributes<HTMLSelectElement>,
+    HTMLSelectElement
+  >
 }
 const SelectLabelAbove: FC<SelectLabelAboveProps> = ({
   onChange,
@@ -23,21 +31,21 @@ const SelectLabelAbove: FC<SelectLabelAboveProps> = ({
   options,
   error,
   selectProps,
-  register,
-}) => {
+  register
+}): JSX.Element => {
   const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>): void => {
-    onChange?.(e);
-    onChangeOption?.(e.currentTarget.value);
-  };
+    onChange?.(e)
+    onChangeOption?.(e.currentTarget.value)
+  }
 
   return (
     <>
-      <p className={style.selectTitle}>{title}</p>
-      <div className={style.selectContainer}>
+      <p className={style.select_title}>{title}</p>
+      <div className={style.select_container}>
         <select
           name={name}
           onChange={onChangeCallback}
-          className={style.selectField}
+          className={style.select_field}
           {...selectProps}
           {...register}
         >
@@ -49,19 +57,19 @@ const SelectLabelAbove: FC<SelectLabelAboveProps> = ({
           {options
             ? options.map((el, i) => {
                 return (
-                  <option value={el} className={style.selectOption} key={i}>
+                  <option value={el} className={style.select_option} key={i}>
                     {el}
                   </option>
-                );
+                )
               })
             : []}
         </select>
-        <span className={style.selectArrow}>&#9660;</span>
+        <span className={style.select_arrow}>&#9660;</span>
       </div>
 
-      {error && <p className={style.selectError}>&#9888; {error}</p>}
+      {error && <p className={style.select_error}>&#9888; {error}</p>}
     </>
-  );
-};
+  )
+}
 
-export default SelectLabelAbove;
+export default SelectLabelAbove
