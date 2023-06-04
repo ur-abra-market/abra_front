95040e379988d52cee91ff37138a3088ce5e85f9
Nikita1025 <nekit1025l@gmail.com>
2023-06-04 18:38:05 +0300

    refactor: validation in SupplierBusinessProfileForm

--- src/common/constants/validation-schemas/supplierBusinessProfileFormValidationSchema.ts
+++ src/common/constants/validation-schemas/supplierBusinessProfileFormValidationSchema.ts
@@ -13,13 +13,19 @@ export const supplierBusinessProfileFormValidationSchema = yup
       .min(2)
       .max(100)
       .typeError('Should be min 2 symbols')
-      .required('Field is required'),
+      .required('Field is required')
+      .test('no-spaces', 'Please enter a valid text', value => {
+        return value === undefined || value.trim() !== '';
+      }),
     entrepreneurNumber: yup
       .string()
       .min(2)
       .max(50)
       .typeError('Should be min 2 symbols')
-      .required('Field is required'),
+      .required('Field is required')
+      .test('no-spaces', 'Please enter a valid text', value => {
+        return value === undefined || value.trim() !== '';
+      }),
     yearEstablished: yup
       .number()
       .min(4, 'Add an existing year')
@@ -27,13 +33,22 @@ export const supplierBusinessProfileFormValidationSchema = yup
       .typeError('Should be number')
       .required('Field is required'),
     email: emailValidationSchema,
-    textarea: yup.string().max(1000).required('Field is required'),
+    textarea: yup
+      .string()
+      .max(1000)
+      .required('Field is required')
+      .test('no-spaces', 'Please enter a valid text', value => {
+        return value === undefined || value.trim() !== '';
+      }),
     address: yup
       .string()
       .min(10)
       .typeError('Should be min 10 symbols')
       .max(300)
-      .required('Field is required'),
+      .required('Field is required')
+      .test('no-spaces', 'Please enter a valid text', value => {
+        return value === undefined || value.trim() !== '';
+      }),
     numEmployees: yup.string().required('Field is required'),
     businessSector: yup.string().required('Field is required'),
   })
