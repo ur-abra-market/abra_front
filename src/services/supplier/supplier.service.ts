f777103545c2ab7340a5755ec51dd51379b04245
sashaChumasov <sashachumasov@yahoo.com>
2023-06-05 17:28:10 +0300

    refactor: added suppliers response types

--- src/services/supplier/supplier.service.ts
+++ src/services/supplier/supplier.service.ts
@@ -1,34 +1,49 @@
 import baseConfigService from '../baseConfig.service';
 
-import { INotification } from './supplier.serviceTypes';
+import {
+  INotification,
+  INotificationData,
+  ISuppliersCompanyData,
+  SuppliersResponse,
+} from './supplier.serviceTypes';
 
 export const supplierService = {
   hasCompanyInfo: async () => {
-    const { data } = await baseConfigService.get(`/suppliers/hasCompanyInfo/`);
+    const { data } = await baseConfigService.get<SuppliersResponse<boolean>>(
+      `/suppliers/hasCompanyInfo/`,
+    );
 
     return data.result;
   },
 
   hasPersonalInfo: async () => {
-    const { data } = await baseConfigService.get(`/suppliers/hasPersonalInfo/`);
+    const { data } = await baseConfigService.get<SuppliersResponse<boolean>>(
+      `/suppliers/hasPersonalInfo/`,
+    );
 
     return data.result;
   },
 
   fetchCompanyLogo: async () => {
-    const { data } = await baseConfigService.get(`/suppliers/companyLogo`);
+    const { data } = await baseConfigService.get<SuppliersResponse<string>>(
+      `/suppliers/companyLogo`,
+    );
 
     return data.result;
   },
 
   fetchCompanyInfo: async () => {
-    const { data } = await baseConfigService.get(`/suppliers/companyInfo`);
+    const { data } = await baseConfigService.get<
+      SuppliersResponse<ISuppliersCompanyData>
+    >(`/suppliers/companyInfo`);
 
     return data.result;
   },
 
   fetchNotifications: async () => {
-    const { data } = await baseConfigService.get(`/suppliers/notifications/`);
+    const { data } = await baseConfigService.get<SuppliersResponse<INotificationData>>(
+      `/suppliers/notifications/`,
+    );
 
     return data.result;
   },
--- src/services/supplier/supplier.serviceTypes.ts
+++ src/services/supplier/supplier.serviceTypes.ts
@@ -43,3 +43,43 @@ export interface INotification {
   on_your_favorites_new: boolean;
   on_account_support: boolean;
 }
+
+export interface SuppliersResponse<R> {
+  ok: boolean;
+  result: R;
+}
+
+export interface ISuppliersCompanyData {
+  id: number;
+  license_number: string;
+  grade_average: number;
+  additional_info: string;
+  company: ICompanyInfoData;
+}
+
+export interface ICompanyInfoData {
+  id: number;
+  business_email: string;
+  name: string;
+  is_manufacturer: boolean;
+  year_established: number;
+  number_employees: number;
+  description: string;
+  address: string;
+  logo_url: string;
+  business_sector: string;
+  images: any[];
+}
+
+export interface INotificationData {
+  id: number;
+  on_advertising_campaigns: boolean;
+  on_order_updates: boolean;
+  on_order_reminders: boolean;
+  on_product_updates: boolean;
+  on_product_reminders: boolean;
+  on_reviews_of_products: boolean;
+  on_change_in_demand: boolean;
+  on_advice_from_abra: boolean;
+  on_account_support: boolean;
+}
