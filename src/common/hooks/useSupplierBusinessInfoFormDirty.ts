dc2384612e7a4c76e17f6a0fe122fa542458da97
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-06-28 12:57:39 +0300

    refactor: fix SupplierBusinessInfoChangeForm

--- src/common/hooks/useSupplierBusinessInfoFormDirty.ts
+++ src/common/hooks/useSupplierBusinessInfoFormDirty.ts
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
+
 import { ISupplierBusinessInfoFormData } from 'common/types';
 import { parsePhoneNumber } from 'common/utils/parsePhoneNumber';
-import { ISupplierBusinessInfo } from 'store/reducers/supplier/profile/slice';
 
 export const useSupplierBusinessInfoFormDirty = (
   data: ISupplierBusinessInfoFormData,
   watch: any,
 ): { isDirty: boolean; isPhoneNumberDisable: boolean } => {
-  const formValues: ISupplierBusinessInfo = watch();
+  const [isDirty, setIsDirty] = useState(true);
+  const formValues: ISupplierBusinessInfoFormData = watch();
+
+  useEffect(() => {
+    const isFormModified = Object.keys(formValues).some(key => {
+      if (key !== 'businessSector' && key !== 'phoneNumber') {
+        return (
+          String(formValues[key as keyof ISupplierBusinessInfoFormData]) !==
+          String(data[key as keyof ISupplierBusinessInfoFormData])
+        );
+      }
+      if (key === 'businessSector') {
+        return data.businessSector?.value !== formValues.businessSector?.value;
+      }
+
+      return false;
+    });
+
+    setIsDirty(isFormModified);
+  }, [formValues, data]);
 
   const serverPhoneNumber = `${data.countryCode}${data.phoneNumber}`;
   const { numberFull: currentPhoneNumber } = parsePhoneNumber(
@@ -14,18 +34,6 @@ export const useSupplierBusinessInfoFormDirty = (
   );
 
   const isPhoneNumberDisable = currentPhoneNumber === serverPhoneNumber;
-
-  const isDirty =
-    data.businessSector?.value !== formValues.businessSector?.value ||
-    data.yearEstablished !== Number(formValues.yearEstablished) ||
-    data.storeName !== formValues.storeName ||
-    data.license !== formValues.license ||
-    data.numEmployees !== Number(formValues.numEmployees) ||
-    data.countryRegistration !== formValues.countryRegistration ||
-    data.description !== formValues.description ||
-    data.email !== formValues.email ||
-    data.address !== formValues.address ||
-    data.countryId !== formValues.countryId;
 
   return { isDirty, isPhoneNumberDisable };
 };
