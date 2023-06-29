a61e3d4545cbf3e56ac8b0d5e49cf0f893808078
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-06-29 18:26:11 +0400

    feat: add validatePhoneNumber

--- src/elements/Phone/PhoneNumber.tsx
+++ src/elements/Phone/PhoneNumber.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import style from './PhoneNumber.module.scss';
+import { PhoneCountryShortType, formatPhoneNumber, validatePhoneNumber } from './utils';
 
 import { useAppSelector } from 'common/hooks';
 import { Input, ISelectOption, Select } from 'ui-kit';
@@ -22,7 +23,7 @@ export const PhoneNumber = (): JSX.Element => {
     value: 5,
   });
   const [phoneNumberBody, setPhoneNumberBody] = useState('');
-  const [phoneCountryShort, setPhoneCountryShort] = useState<CountryCode>('ru');
+  const [phoneCountryShort, setPhoneCountryShort] = useState<PhoneCountryShortType>('az');
   const {
     handleSubmit,
     register,
@@ -55,254 +56,39 @@ export const PhoneNumber = (): JSX.Element => {
   const handlePhoneCountryCodeOnChange = (value: ISelectOption): void => {
     const country = countries.find(country => country.id === value.value);
 
-    if (country) setPhoneCountryShort(country.country_short as CountryCode);
+    if (country) setPhoneCountryShort(country.country_short as PhoneCountryShortType);
     setPhoneCountryCode(value);
-  };
-
-  type CountryCode = 'ru' | 'az' | 'by' | 'kz' | 'kg' | 'tr' | 'tj' | 'ua' | 'uz';
-  const validatePhoneNumber = (
-    phoneNumber: string,
-    phoneNumberCountyCode: CountryCode,
-  ): boolean => {
-    const countryPhoneRegex: Record<CountryCode, RegExp> = {
-      ru: /^9\d{9}$/, // Russia: phone number body starts with 9 and has a length of 10
-      az: /^[4567]\d{8}$/, // Azerbaijan: phone number body starts with 4, 5, 6, or 7 and has a length of 9
-      by: /^[2345]\d{8}$/, // Belarus: phone number body starts with 2, 3, 4, or 5 and has a length of 9
-      kz: /^7\d{9}$/, // Kazakhstan: phone number body starts with 7 and has a length of 10
-      kg: /^[57]\d{9}$/, // Kyrgyzstan: phone number body starts with 5 or 7 and has a length of 10
-      tr: /^5\d{9}$/, // Turkey: phone number body starts with 5 and has a length of 10
-      tj: /^\d{9}$/, // Tajikistan: phone number body has a length of 9
-      ua: /^\d{9}$/, // Ukraine: phone number body has a length of 9
-      uz: /^\d{9}$/, // Uzbekistan: phone number body has a length of 9
-    };
-
-    const regex = countryPhoneRegex[phoneNumberCountyCode];
-
-    return regex.test(phoneNumber);
   };
 
   const getFormattedPhoneNumberBody = (
     phoneNumberBody: string,
-    phoneNumberCountyCode: string,
-  ): string => {
-    const changedNumberBody = phoneNumberBody;
-
-    if (phoneNumberCountyCode === 'ru') {
-      // россия
-      if (phoneNumberBody.length <= 10) {
-        return changedNumberBody.replace(
-          /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-    if (phoneNumberCountyCode === 'az') {
-      // азербайджан
-      if (phoneNumberBody.length <= 9) {
-        return changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'by') {
-      // беларусь
-      if (phoneNumberBody.length <= 9) {
-        return changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'kz') {
-      // казахстан
-      if (phoneNumberBody.length <= 10) {
-        return changedNumberBody.replace(
-          /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'kg') {
-      // Киргизия
-      if (phoneNumberBody.length <= 9) {
-        return changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'tj') {
-      // Таджикистан
-      if (phoneNumberBody.length <= 9) {
-        return changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'ua') {
-      // Украина
-      if (phoneNumberBody.length <= 9) {
-        return changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'tr') {
-      // Турция
-      if (phoneNumberBody.length <= 10) {
-        return changedNumberBody.replace(
-          /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    if (phoneNumberCountyCode === 'uz') {
-      // Узбекистан
-      if (phoneNumberBody.length <= 9) {
-        changedNumberBody.replace(
-          /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/,
-          (match, g1, g2, g3, g4) => {
-            let result = '';
-
-            result += g1 ? `${g1}` : '';
-            result += g2 ? ` ${g2}` : '';
-            result += g3 ? `-${g3}` : '';
-            result += g4 ? `-${g4}` : '';
-
-            return result;
-          },
-        );
-      }
-
-      return '';
-    }
-
-    return '';
-  };
-
-  console.log(phoneCountryShort);
+    phoneNumberCountryCode: PhoneCountryShortType,
+  ): string => formatPhoneNumber(phoneNumberBody, phoneNumberCountryCode);
 
   const onChangePhoneNumberBody = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const inputValue = e.currentTarget.value;
-    const rawValue = inputValue.replace(/\D/g, ''); // оставляем только цифры
+    const phoneNumberBodyRawValue = e.currentTarget.value.replace(/\D/g, ''); // оставляем из пришедшего значения инпута только цифры
 
-    if (!rawValue && phoneNumberBody) {
-      // если приходит значение пустое, и при этом текущее значение имеет данные - значит мы пытаемся очистить инпут телефона и надо засетаить пустую строку и выкинуть ошибку
+    if (!phoneNumberBodyRawValue && phoneNumberBody) {
+      // если пришла пуста строка вместо цифр и при этом есть текущее значение тела номера телефона - значит пользователь хочет очистить инпут телефона. поэтому мы сохраняем пустую строку в тело номера телефона и выкидываем ошибку
       setPhoneNumberBody('');
       setError('phoneNumberBody', { message: 'Please, enter a valid phone number' });
-    } else {
-      const formattedNumber = getFormattedPhoneNumberBody(rawValue, phoneCountryShort); // получаем отформатированное по маске страны с countryCode тело телефонного номера
 
-      if (formattedNumber) {
-        setPhoneNumberBody(formattedNumber);
-        if (!validatePhoneNumber(rawValue, phoneCountryShort)) {
-          setError('phoneNumberBody', { message: 'Please, enter a valid phone number' });
-        } else {
-          clearErrors('phoneNumberBody');
-        }
-      }
+      return;
+    }
+
+    const formattedNumber = getFormattedPhoneNumberBody(
+      phoneNumberBodyRawValue,
+      phoneCountryShort,
+    ); // получаем отформатированное по маске страны с countryCode тело телефонного номера
+
+    if (!formattedNumber) return; // если вернулась пустая строка (то есть телефон превысил значение маски) тогда мы ничего не будем сохранять в инпут и выводить на ui
+
+    setPhoneNumberBody(formattedNumber); // в противном случае сохраняем значение на ui
+
+    if (!validatePhoneNumber(phoneNumberBodyRawValue, phoneCountryShort)) {
+      setError('phoneNumberBody', { message: 'Please, enter a valid phone number' });
+    } else {
+      clearErrors('phoneNumberBody');
     }
   };
 
---
+++ src/elements/Phone/utils.ts
@@ -0,0 +1,99 @@
+export type PhoneCountryShortType =
+  | 'ru'
+  | 'az'
+  | 'by'
+  | 'kz'
+  | 'kg'
+  | 'tr'
+  | 'tj'
+  | 'ua'
+  | 'uz';
+
+interface ICountryPhoneInfo {
+  maxPhoneLength: number;
+  startRegex: RegExp;
+  maskRegex: RegExp;
+}
+
+const countryPhoneInfo: Record<PhoneCountryShortType, ICountryPhoneInfo> = {
+  ru: {
+    maxPhoneLength: 10,
+    startRegex: /^9\d{9}$/,
+    maskRegex: /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  az: {
+    maxPhoneLength: 9,
+    startRegex: /^[4567]\d{8}$/,
+    maskRegex: /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  by: {
+    maxPhoneLength: 9,
+    startRegex: /^[2345]\d{8}$/,
+    maskRegex: /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  kz: {
+    maxPhoneLength: 10,
+    startRegex: /^7\d{9}$/,
+    maskRegex: /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  kg: {
+    maxPhoneLength: 10,
+    startRegex: /^[57]\d{9}$/,
+    maskRegex: /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  tr: {
+    maxPhoneLength: 10,
+    startRegex: /^5\d{9}$/,
+    maskRegex: /^(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  tj: {
+    maxPhoneLength: 9,
+    startRegex: /^\d{9}$/,
+    maskRegex: /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  ua: {
+    maxPhoneLength: 9,
+    startRegex: /^\d{9}$/,
+    maskRegex: /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+  uz: {
+    maxPhoneLength: 9,
+    startRegex: /^\d{9}$/,
+    maskRegex: /^(\d{1,2})?(\d{1,3})?(\d{1,2})?(\d{1,2})?$/,
+  },
+};
+
+export const validatePhoneNumber = (
+  phoneNumber: string,
+  phoneNumberCountryCode: PhoneCountryShortType,
+): boolean => {
+  const countryInfo = countryPhoneInfo[phoneNumberCountryCode];
+
+  if (!countryInfo) {
+    return false;
+  }
+
+  return countryInfo.startRegex.test(phoneNumber);
+};
+
+export const formatPhoneNumber = (
+  phoneNumberBody: string,
+  phoneNumberCountryCode: PhoneCountryShortType,
+): string => {
+  const countryInfo = countryPhoneInfo[phoneNumberCountryCode];
+
+  if (!countryInfo || phoneNumberBody.length > countryInfo.maxPhoneLength) {
+    return '';
+  }
+
+  return phoneNumberBody.replace(countryInfo.maskRegex, (match, g1, g2, g3, g4) => {
+    let result = '';
+
+    result += g1 ? `${g1}` : '';
+    result += g2 ? ` ${g2}` : '';
+    result += g3 ? `-${g3}` : '';
+    result += g4 ? `-${g4}` : '';
+
+    return result;
+  });
+};
