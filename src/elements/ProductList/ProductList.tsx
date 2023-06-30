a7cdfc821d324770b34bc91681c4b2b382787a7b
Grigory Dobrenko <grigorydobrenko97@gmail.com>
2023-06-30 17:33:21 +0300

    fix: fix layouts

--- src/elements/ProductList/ProductList.tsx
+++ src/elements/ProductList/ProductList.tsx
@@ -64,6 +64,102 @@ export const ProductList = (): JSX.Element => {
       ],
     },
     {
+      id: 3651,
+      name: 'Why lead condition individual analysis.',
+      description: 'What white fall individual century imagine recently.',
+      datetime: '2023-06-28T09:07:02.025924+00:00',
+      grade_average: 4.9,
+      total_orders: 0,
+      uuid: '145b5369-0a1a-4f0b-9fec-26df2fc3d751',
+      is_active: true,
+      supplier: {
+        id: 14,
+        license_number: '7164018933195',
+        grade_average: 4.4,
+        additional_info:
+          'Behavior bag probably window seven high war. Above myself decide institution hand loss the. Put end land represent room office site. Teach walk discuss loss. Force agreement sport but catch structure. Draw difficult operation especially line from.',
+        user: {
+          id: 30,
+          datetime: '2023-06-28T09:06:43.709813+00:00',
+          phone_number: '9738476023429',
+          first_name: 'Chad',
+          last_name: 'Chandler',
+          full_name: 'Chad Chandler',
+          email: '290060stephanie45@example.net',
+          is_verified: true,
+          is_deleted: false,
+          is_supplier: true,
+        },
+      },
+      images: [
+        {
+          id: 11047,
+          image_url:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlLPA81SVi6SpC3wjtHFAhLekjl50IrC9pml04-6Qj&s',
+          // image_url: 'https://picsum.photos/id/197/220/220',
+          order: 1,
+        },
+      ],
+      prices: [
+        {
+          id: 3651,
+          value: 3893.36,
+          discount: 0.13,
+          min_quantity: 64,
+          start_date: '2023-06-28T09:07:02.026840+00:00',
+          end_date: '2026-01-07T09:07:02.026842+00:00',
+        },
+      ],
+    },
+    {
+      id: 3651,
+      name: 'Why lead condition individual analysis.',
+      description: 'What white fall individual century imagine recently.',
+      datetime: '2023-06-28T09:07:02.025924+00:00',
+      grade_average: 4.9,
+      total_orders: 0,
+      uuid: '145b5369-0a1a-4f0b-9fec-26df2fc3d751',
+      is_active: true,
+      supplier: {
+        id: 14,
+        license_number: '7164018933195',
+        grade_average: 4.4,
+        additional_info:
+          'Behavior bag probably window seven high war. Above myself decide institution hand loss the. Put end land represent room office site. Teach walk discuss loss. Force agreement sport but catch structure. Draw difficult operation especially line from.',
+        user: {
+          id: 30,
+          datetime: '2023-06-28T09:06:43.709813+00:00',
+          phone_number: '9738476023429',
+          first_name: 'Chad',
+          last_name: 'Chandler',
+          full_name: 'Chad Chandler',
+          email: '290060stephanie45@example.net',
+          is_verified: true,
+          is_deleted: false,
+          is_supplier: true,
+        },
+      },
+      images: [
+        {
+          id: 11047,
+          image_url:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlLPA81SVi6SpC3wjtHFAhLekjl50IrC9pml04-6Qj&s',
+          // image_url: 'https://picsum.photos/id/197/220/220',
+          order: 1,
+        },
+      ],
+      prices: [
+        {
+          id: 3651,
+          value: 3893.36,
+          discount: 0.13,
+          min_quantity: 64,
+          start_date: '2023-06-28T09:07:02.026840+00:00',
+          end_date: '2026-01-07T09:07:02.026842+00:00',
+        },
+      ],
+    },
+    {
       id: 4527,
       name: 'Still feeling.',
       description: 'Friend hotel realize though class space whom best.',
