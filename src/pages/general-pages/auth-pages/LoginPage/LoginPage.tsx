2debf358af34f12a9e78914d19169b681796ae93
sashaChumasov <sashachumasov@yahoo.com>
2023-07-02 15:46:10 +0300

    refactor: change types IAuthPageLayout to add new links

--- src/pages/general-pages/auth-pages/LoginPage/LoginPage.tsx
+++ src/pages/general-pages/auth-pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,17 @@
 import { LoginForm } from '.';
 
+import { IAuthFooterData } from 'common/types';
 import { AuthPageLayout } from 'pages/general-pages/auth-pages/assets';
-import { FORGOT_PASSWORD } from 'routes';
+import { FORGOT_PASSWORD, REGISTER } from 'routes';
 
 export const LoginPage = (): JSX.Element => {
+  const loginFooterData: IAuthFooterData[] = [
+    { link: FORGOT_PASSWORD, title: 'Forgot password' },
+    { link: REGISTER, title: 'Create account' },
+  ];
+
   return (
-    <AuthPageLayout
-      isMainLogoShow
-      footerLink={FORGOT_PASSWORD}
-      footerTitle="Forgot password"
-    >
+    <AuthPageLayout isMainLogoShow footerData={loginFooterData}>
       <LoginForm />
     </AuthPageLayout>
   );
