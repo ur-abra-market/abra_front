015b2f5cdafb5260387697cad394f5649a138677
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-07-05 21:05:42 +0300

    feat: add structure

---
+++ src/pages/seller-pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import style from './ProductPage.module.scss';
+
+export const ProductPage = (): JSX.Element => {
+  return <div className={style.product_container} />;
+};
---
+++ src/pages/seller-pages/ProductPage/ProductPageHeader/ProductPageHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import style from './ProductPageHeader.module.scss';
+
+export const ProductPageHeader = (): JSX.Element => {
+  return <header />;
+};
