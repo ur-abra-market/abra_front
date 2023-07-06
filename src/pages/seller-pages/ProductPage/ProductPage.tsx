727e3d2ae5fffc98a46f977b3b773e8acdf1ab65
Sergey_Saprankov <sergey-saprankov@mail.ru>
2023-07-06 21:49:57 +0300

    refactor: rename

--- src/pages/seller-pages/ProductPage/ProductPage.tsx
+++ src/pages/seller-pages/ProductPage/ProductPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import { ProductMainInfo } from './ProductInfo/ProductInfo';
+import { ProductMainInfo } from './ProductMainInfo/ProductMainInfo';
 import style from './ProductPage.module.scss';
 import { ProductPageHeader } from './ProductPageHeader/ProductPageHeader';
 
