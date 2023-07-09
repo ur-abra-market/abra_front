a8cd1e4d044374c688183e949691489edf266332
Sergey_Saprankov <sergey-saprankov@mail.ru>
2023-07-09 18:14:36 +0300

    feat: ProductOverview

--- src/pages/seller-pages/ProductPage/ProductMainInfo/ProductDetails/ProductDetails.tsx
+++ src/pages/seller-pages/ProductPage/ProductMainInfo/ProductDetails/ProductDetails.tsx
@@ -5,16 +5,11 @@ import style from './ProductDetails.module.scss';
 import { useAppSelector } from 'common/hooks';
 import { Tags } from 'elements';
 // import { ProductColor } from 'elements';
-import {
-  productNameSelector,
-  productTagsSelector,
-  productVariationsSelector,
-} from 'store/reducers/productSliceNew';
+import { productNameSelector, productTagsSelector } from 'store/reducers/productSliceNew';
 
 export const ProductDetails = (): JSX.Element => {
   const title = useAppSelector(productNameSelector);
   const tags = useAppSelector(productTagsSelector);
-  const variations = useAppSelector(productVariationsSelector); // это пока полная ерунда, бэк отправляет всё в кучу в этот массив
 
   return (
     <div className={style.product_details_container}>
