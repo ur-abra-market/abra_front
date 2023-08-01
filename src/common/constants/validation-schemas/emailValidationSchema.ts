a82b1852cdcfdca41d7ede4bb7389965ff2b0783
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-08-01 19:16:23 +0400

    fix: fix email validation

--- src/common/constants/validation-schemas/emailValidationSchema.ts
+++ src/common/constants/validation-schemas/emailValidationSchema.ts
@@ -2,5 +2,6 @@ import * as yup from 'yup';
 
 export const emailValidationSchema = yup
   .string()
+  .matches(/@/, 'You forgot about “@”')
   .email('Invalid email')
   .required('Email is required');
