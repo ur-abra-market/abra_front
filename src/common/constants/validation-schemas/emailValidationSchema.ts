a23ce9e1d6346b16d2e6fc008bdabcd1ae2210a6
L3GO321 <googlik701@gmail.com>
2023-08-13 18:46:03 +0300

    fixes

--- src/common/constants/validation-schemas/emailValidationSchema.ts
+++ src/common/constants/validation-schemas/emailValidationSchema.ts
@@ -5,6 +5,6 @@ const emailRegex =
 
 export const emailValidationSchema = yup
   .string()
-  .matches(/@/, 'You forgot about “@”')
-  .email('Invalid email')
+  .trim()
+  .matches(emailRegex, 'Invalid email')
   .required('Email is required');
