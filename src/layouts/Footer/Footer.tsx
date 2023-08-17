55e12285af084d415abd98db251be859b32d8862
cgrn24 <stlhnt42@gmail.com>
2023-08-18 03:11:02 +0700

    refactor: footer without two variants

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -26,6 +26,11 @@ export const Footer: FC<IFooter> = ({ className, variant }): JSX.Element => {
     [style.footer_white]: variant === 'white',
   });
 
+  const linksStyle = cn({
+    [style.links]: variant === 'default',
+    [style.links_white]: variant === 'white',
+  });
+
   return (
     <div className={cn(style.wrapper, className)}>
       {isShowTopNav && <Top />}
@@ -33,50 +38,39 @@ export const Footer: FC<IFooter> = ({ className, variant }): JSX.Element => {
       <div className={footerClasses}>
         <div className={style.container}>
           <div className={style.flex_box}>
-            {variant === 'default' ? (
-              <>
-                <div className={style.links}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      isActive ? style.is_disabled : style.link
-                    }
-                    to={TERMS_AND_CONDITIONS}
-                  >
-                    Terms & conditions
-                  </NavLink>
-                  <NavLink
-                    className={({ isActive }) =>
-                      isActive ? style.is_disabled : style.link
-                    }
-                    to={PRIVACY_POLICY}
-                  >
-                    Privacy policy
-                  </NavLink>
-                </div>
-                <div className={style.copyright}>
-                  <span>&#169; Copyright 2023</span>
-                </div>
-              </>
-            ) : (
-              <div className={style.links_white}>
-                &#169; 2022 Abra.
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? style.is_disabled_white : style.link_white
-                  }
-                  to={TERMS_AND_CONDITIONS}
-                >
-                  Terms & conditions
-                </NavLink>
-                and&nbsp;
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? style.is_disabled_white : style.link_white
-                  }
-                  to={PRIVACY_POLICY}
-                >
-                  Privacy policy
-                </NavLink>
+            <div className={linksStyle}>
+              {variant === 'white' && <span>&#169; 2022 Abra.</span>}
+              <NavLink
+                className={({ isActive }) =>
+                  cn({
+                    [style.is_disabled]: variant === 'default' && isActive,
+                    [style.is_disabled_white]: variant === 'white' && isActive,
+                    [style.link]: variant === 'default',
+                    [style.link_white]: variant === 'white',
+                  })
+                }
+                to={TERMS_AND_CONDITIONS}
+              >
+                Terms & conditions
+              </NavLink>
+              {variant === 'white' && <span>and&nbsp;</span>}
+              <NavLink
+                className={({ isActive }) =>
+                  cn({
+                    [style.is_disabled]: variant === 'default' && isActive,
+                    [style.is_disabled_white]: variant === 'white' && isActive,
+                    [style.link]: variant === 'default',
+                    [style.link_white]: variant === 'white',
+                  })
+                }
+                to={PRIVACY_POLICY}
+              >
+                Privacy policy
+              </NavLink>
+            </div>
+            {variant === 'default' && (
+              <div className={style.copyright}>
+                <span>&#169; Copyright 2023</span>
               </div>
             )}
           </div>
