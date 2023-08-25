d3f9e2cce7fff9db76d382caa21387f0745dc037
yaromahob <82041613+yaromahob@users.noreply.github.com>
2023-08-25 11:51:03 +0300

    feat: add keyboard control for categories menu (#283)

---
+++ src/common/constants/keyboard-schema/KEYBOARD_KEYS.ts
@@ -0,0 +1,8 @@
+export const KEYBOARD_KEYS = {
+  ENTER: 'Enter',
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown',
+  ARROW_LEFT: 'ArrowLeft',
+  ARROW_RIGHT: 'ArrowRight',
+  ESCAPE: 'Escape',
+};
