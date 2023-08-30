883b05f5e6ee3b3a8ec8fe8b5b7809e653442bfc
yaromahob <yaromahob@gmail.com>
2023-08-30 23:02:02 +0300

    feat: add edits and correct button question

--- src/pages/general-pages/MainPage/SubscriptionAndContacts/SubscriptionAndContacts.tsx
+++ src/pages/general-pages/MainPage/SubscriptionAndContacts/SubscriptionAndContacts.tsx
@@ -14,20 +14,21 @@ export const SubscriptionAndContacts = (): JSX.Element => {
             <div className={style.subscribe_subtitle}>
               to know about new products and hype products?
             </div>
-            <div className={style.input_box}>
-              <Input placeholder="Enter your email address" />
-              <Button label="Subscribe" />
-            </div>
+          </div>
+          <div className={style.input_box}>
+            <Input placeholder="Enter your email address" />
+            <Button label="Subscribe" />
           </div>
         </div>
+        <div className={style.our_media}>
+          <div className={style.feedback_wrapper}>
+            <div className={style.phone_title}>Contact Us via WhatsApp</div>
+            <WhatsappPhoneNumber />
+          </div>
 
-        <div className={style.feedback_wrapper}>
-          <div className={style.phone_title}>Contact Us via WhatsApp</div>
-          <WhatsappPhoneNumber />
-        </div>
-
-        <div className={style.social_network_wrapper}>
-          <SocialNetworks />
+          <div className={style.social_network_wrapper}>
+            <SocialNetworks />
+          </div>
         </div>
       </div>
     </div>
