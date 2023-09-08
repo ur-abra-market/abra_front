9d155926db68d20ddd8cdc93fd6084d5448a976b
yaromahob <82041613+yaromahob@users.noreply.github.com>
2023-09-08 14:17:54 +0300

    feat: adaptive footer main page (#299)

--- src/pages/general-pages/MainPage/SubscriptionAndContacts/SubscriptionAndContacts.tsx
+++ src/pages/general-pages/MainPage/SubscriptionAndContacts/SubscriptionAndContacts.tsx
@@ -8,23 +8,28 @@ export const SubscriptionAndContacts = (): JSX.Element => {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        <div>
-          <div className={style.subscribe_title}>Do you want to be the first</div>
-          <div className={style.subscribe_subtitle}>
-            to know about new products and hype products?
+        <div className={style.subscribe_wrapper}>
+          <div className={style.text}>
+            <div className={style.subscribe_title}>Do you want to be the first</div>
+            <div className={style.subscribe_subtitle}>
+              to know about new products and hype products?
+            </div>
           </div>
           <div className={style.input_box}>
             <Input placeholder="Enter your email address" />
             <Button label="Subscribe" />
           </div>
         </div>
+        <div className={style.our_media}>
+          <div className={style.feedback_wrapper}>
+            <div className={style.phone_title}>Contact Us via WhatsApp</div>
+            <WhatsappPhoneNumber />
+          </div>
 
-        <div>
-          <div className={style.phone_title}>Contact Us via WhatsApp</div>
-          <WhatsappPhoneNumber />
+          <div className={style.social_network_wrapper}>
+            <SocialNetworks />
+          </div>
         </div>
-
-        <SocialNetworks />
       </div>
     </div>
   );
