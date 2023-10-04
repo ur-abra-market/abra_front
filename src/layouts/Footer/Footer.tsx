c126b93818a8015b47a61aacfbe1c7e2d75cb0ef
Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-10-04 15:40:31 +0400

    fix: fix footer styles

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -10,11 +10,11 @@ import { Top } from '.';
 import { useAppSelector } from 'common/hooks';
 import { PRIVACY_POLICY, TERMS_AND_CONDITIONS } from 'routes';
 import { userRoleSelector } from 'store/reducers/authSlice';
-import { Paragraph } from 'ui-kit';
 
 interface IFooter
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  variant: 'white' | 'default';
+  variant: 'white' | 'black';
+  className?: string;
 }
 
 export const Footer: FC<IFooter> = ({ className, variant }): JSX.Element => {
@@ -23,44 +23,28 @@ export const Footer: FC<IFooter> = ({ className, variant }): JSX.Element => {
   const userRole = useAppSelector(userRoleSelector);
   const isShowTopNav = userRole === 'seller' && routesToShow.has(pathname.split('/')[1]);
 
-  const footerClasses = cn(style.footer, {
-    [style.footer_white]: variant === 'white',
-  });
-
-  const linksClasses = cn({
-    [style.links]: variant === 'default',
-    [style.links_white]: variant === 'white',
-  });
-
-  const navLinkClasses = cn(style.link, {
-    [style.link_white]: variant === 'white',
+  const footerClasses = cn(style.footer_bottom_wrapper, {
+    [style.footer_bottom_black]: variant === 'black',
+    [style.footer_bottom_white]: variant === 'white',
   });
 
   return (
-    <footer className={cn(style.wrapper, className)}>
+    <footer className={className || ''}>
       {isShowTopNav && <Top />}
 
       <div className={footerClasses}>
-        <div className={style.container}>
-          {variant === 'default' && (
-            <Paragraph className={style.copyright} size="s2">
-              &#169; Abra 2023
-            </Paragraph>
-          )}
+        <div className={style.footer_bottom}>
+          <p className={style.copyright}>&#169; 2023 Abra.</p>
 
-          <div className={linksClasses}>
-            {variant === 'white' && <Paragraph size="xs2">&#169; 2023 Abra.</Paragraph>}
-            <NavLink className={navLinkClasses} to={TERMS_AND_CONDITIONS}>
-              Terms & conditions
-            </NavLink>
+          <NavLink className={style.link} to={TERMS_AND_CONDITIONS}>
+            Terms & conditions
+          </NavLink>
 
-            {variant === 'white' && (
-              <span className={style.link_separator}>and&nbsp;</span>
-            )}
-            <NavLink className={navLinkClasses} to={PRIVACY_POLICY}>
-              Privacy policy
-            </NavLink>
-          </div>
+          <span className={style.links_separator}>&nbsp;and&nbsp;</span>
+
+          <NavLink className={style.link} to={PRIVACY_POLICY}>
+            Privacy policy
+          </NavLink>
         </div>
       </div>
     </footer>
