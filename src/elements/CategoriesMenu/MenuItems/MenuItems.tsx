492b9c079938a32b491faffc914cdaf000b00595
zemosamurai <113992154+zemosamurai@users.noreply.github.com>
2023-10-05 16:51:30 +0300

    feat: adaptive categories menu (#321)

--- src/elements/CategoriesMenu/MenuItems/MenuItems.tsx
+++ src/elements/CategoriesMenu/MenuItems/MenuItems.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 import cn from 'classnames';
 import { NavLink } from 'react-router-dom';
@@ -52,9 +52,5 @@ export const MenuItems: FC<IMenuItems> = ({
     );
   });
 
-  return (
-    <div className={style.items_wrapper}>
-      <div className={cn(style.items_container)}>{mappedItems}</div>
-    </div>
-  );
+  return <div className={cn(style.items_container)}>{mappedItems}</div>;
 };
