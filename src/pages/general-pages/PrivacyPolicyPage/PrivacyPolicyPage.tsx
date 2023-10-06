5bd97458146a91c9f8d6814785a516a6a5cd054a
Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-10-06 13:43:58 +0400

    fix: fix footer styles

--- src/pages/general-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ src/pages/general-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -18,6 +18,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
         <Title as="h1" size="xl">
           Abra&apos;s Privacy Policy
         </Title>
+
         <Title as="h2" size="xs" className={style.title_block}>
           Last Updated: 04/11/2023
         </Title>
@@ -34,6 +35,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           your personal information in accordance with this Privacy Policy. If you do not
           agree with this Privacy Policy, please do not use our Marketplace.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           1. Information We Collect
         </Title>
@@ -62,6 +64,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           collect information about your use of our Marketplace and to remember your
           preferences.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           2. How We Use Your Information
         </Title>
@@ -90,6 +93,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
         <Paragraph size="m" className={style.text}>
           g. Conduct research, analyze data, and improve our Marketplace.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           3. How We Share Your Information
         </Title>
@@ -114,6 +118,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           e. In connection with a merger, acquisition, or sale of assets, subject to
           applicable laws and confidentiality agreements.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           4. Your Rights and Choices
         </Title>
@@ -122,16 +127,25 @@ export const PrivacyPolicyPage = (): JSX.Element => {
         </Paragraph>
         <Paragraph size="m" className={style.text}>
           a. Access, update, or delete your personal information by logging into your
-          account or contacting us at service@abra.com.
+          account or contacting us at&nbsp;
+          <SimpleLink color="accent" to="mailto:service@abra.com">
+            service@abra.com
+          </SimpleLink>
+          .
         </Paragraph>
         <Paragraph size="m" className={style.text}>
           b. Opt-out of marketing communications by clicking the &quot;unsubscribe&quot;
-          link in any promotional email or contacting us at service@abra.com.
+          link in any promotional email or contacting us at&nbsp;
+          <SimpleLink color="accent" to="mailto:service@abra.com">
+            service@abra.com
+          </SimpleLink>
+          .
         </Paragraph>
         <Paragraph size="m" className={style.text}>
           c. Disable cookies in your browser settings, although this may affect your
           experience on our Marketplace.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           5. Data Retention and Security
         </Title>
@@ -141,6 +155,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           required or allowed by law. We take appropriate measures to protect your
           personal information from unauthorized access, loss, and misuse.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           6. Changes to This Privacy Policy
         </Title>
@@ -150,6 +165,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           Your continued use of our Marketplace after the effective date of the revised
           Privacy Policy constitutes your acceptance of the changes.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           7. Contact Us
         </Title>
@@ -158,17 +174,15 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           practices, please contact us at:
         </Paragraph>
         <Paragraph size="m" className={style.text}>
-          Abra
+          Abra, Turkey, Antalya
         </Paragraph>
         <Paragraph size="m" className={style.text}>
-          Turkey, Antalya
-        </Paragraph>
-        <Paragraph size="m" className={style.text}>
-          Email:{' '}
+          Email:&nbsp;
           <SimpleLink color="accent" to="mailto:service@abra.com">
             service@abra.com
           </SimpleLink>
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           8. International Data Transfers
         </Title>
@@ -180,6 +194,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           to ensure that your personal information is treated securely and in accordance
           with this Privacy Policy.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           9. Children&apos;s Privacy
         </Title>
@@ -188,9 +203,14 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           minimum age required in your jurisdiction). We do not knowingly collect personal
           information from children under the age of 13. If you become aware that a child
           has provided us with personal information without parental consent, please
-          contact us at service@abra.com, and we will take steps to remove such
-          information and terminate the child&apos;s account.
+          contact us at&nbsp;
+          <SimpleLink color="accent" to="mailto:service@abra.com">
+            service@abra.com
+          </SimpleLink>
+          , and we will take steps to remove such information and terminate the
+          child&apos;s account.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           10. Third-Party Websites and Services
         </Title>
@@ -201,24 +221,35 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           with third-party websites or services, and we encourage you to review their
           privacy policies before providing any personal information.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           11. Your California Privacy Rights
         </Title>
         <Paragraph size="m" className={style.text}>
           If you are a California resident, you may have certain rights under the
           California Consumer Privacy Act (CCPA). To exercise these rights, please contact
-          us at service@abra.com. Please note that we may need to verify your identity
-          before fulfilling your request.
+          us at&nbsp;
+          <SimpleLink color="accent" to="mailto:service@abra.com">
+            service@abra.com
+          </SimpleLink>
+          . Please note that we may need to verify your identity before fulfilling your
+          request.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           12. Your European Privacy Rights
         </Title>
         <Paragraph size="m" className={style.text}>
           If you are a resident of the European Economic Area (EEA), you have certain
           rights under the General Data Protection Regulation (GDPR). To exercise these
-          rights, please contact us at service@abra.com. Please note that we may need to
-          verify your identity before fulfilling your request.
+          rights, please contact us at&nbsp;
+          <SimpleLink color="accent" to="mailto:service@abra.com">
+            service@abra.com
+          </SimpleLink>
+          . Please note that we may need to verify your identity before fulfilling your
+          request.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           13. Governing Law
         </Title>
@@ -227,6 +258,7 @@ export const PrivacyPolicyPage = (): JSX.Element => {
           governed by and construed in accordance with the laws of Turkey, without regard
           to its conflicts of law principles.
         </Paragraph>
+
         <Title as="h2" size="xs" className={style.title_block}>
           14. Updates to This Privacy Policy
         </Title>
