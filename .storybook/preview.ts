9ae57cc9a5a22e8f8c4f90e40ee9cac78467f434
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-10-12 19:12:04 +0300

    feat: adaptive product page

---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from '@storybook/react';
+import '../src/index.css';
+import { withRouter } from 'storybook-addon-react-router-v6';
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export const decorators = [withRouter];
+export default preview;
