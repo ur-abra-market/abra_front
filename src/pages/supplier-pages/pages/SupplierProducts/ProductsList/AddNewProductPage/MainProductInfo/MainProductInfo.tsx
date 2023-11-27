a88fb7f13703d14239ee2118997393e1de4a84ed
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-11-27 19:48:06 +0300

    Added: Inputs to MainProductInfo

--- src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/MainProductInfo/MainProductInfo.tsx
+++ src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/MainProductInfo/MainProductInfo.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
+import { Controller, useForm } from 'react-hook-form';
+
+import { Label, Input } from 'ui-kit';
+
 export const MainProductInfo: React.FC = () => {
+  const { control } = useForm();
+
   return (
-    <div>
-      <h1>Main Product Info</h1>
-    </div>
+    <form>
+      <div>
+        <Label label="Product name *" htmlFor="productName">
+          <Controller
+            name="productName"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} placeholder="Enter the product name" />
+            )}
+          />
+        </Label>
+        <Label label="Description *" htmlFor="description">
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => <Input {...field} placeholder="Enter description" />}
+          />
+        </Label>
+        <Label label="Brand name *" htmlFor="brandName">
+          <Controller
+            name="brandName"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} placeholder="Select or enter brand name" />
+            )}
+          />
+        </Label>
+      </div>
+    </form>
   );
 };
