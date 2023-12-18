5f87a046bad17e19604a641481edc6d88c0265ac
yaromahob <yaromahob@gmail.com>
2023-12-18 22:08:44 +0300

    fix: button without color

--- src/elements/ProductParams/ProductColor/ProductColor.tsx
+++ src/elements/ProductParams/ProductColor/ProductColor.tsx
@@ -63,9 +63,12 @@ export const ProductColor: FC<IProductColorProps> = ({ colors }): JSX.Element =>
             <li
               onClick={() => setActive(el.id)}
               key={el.id}
-              className={cn(style.item, { [style.active]: el.id === active })}
+              className={cn(style.item, style.without_color, {
+                [style.active]: el.id === active,
+              })}
             >
-              <WithoutColor />
+              {/* <WithoutColor /> */}
+              <div />
             </li>
           ),
         )}
