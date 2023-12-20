85ddf7ab1e225edf524379f8c24b1bef8542428b
Ni-speed <railmir@gmail.com>
2023-12-20 15:10:15 +0500

    feat: add ProductSizeSelector component

---
+++ src/elements/ProductParams/ProductSizeSelector/ProductSizeSelector .tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+
+import cn from 'classnames';
+
+import { SizeEnums } from 'common/types';
+
+import style from './ProductSizeSelector.module.scss';
+
+interface ProductSizeSelectorProps {
+  size: SizeEnums;
+  quantity: number;
+  active: boolean;
+  onClick: () => void;
+}
+export const ProductSizeSelector: FC<ProductSizeSelectorProps> = ({
+  size,
+  active,
+  onClick,
+  quantity,
+}): JSX.Element => {
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+    <li onClick={onClick}>
+      <span className={cn(style.item, { [style.active]: active })}>{size}</span>
+      <span className={style.remains}>{quantity}</span>
+    </li>
+  );
+};
