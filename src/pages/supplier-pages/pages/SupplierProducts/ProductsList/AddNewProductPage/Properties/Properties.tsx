83076818ae188ef1ef30a3500df3fd4792f7ce87
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-12-22 23:04:18 +0300

    Added: Properties block

--- src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Properties/Properties.tsx
+++ src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Properties/Properties.tsx
@@ -1,9 +1,147 @@
 import React, { FC } from 'react';
 
+import { Controller, useForm } from 'react-hook-form';
+
+import { useAppSelector } from 'common/hooks';
+import { Label, Input, Select } from 'ui-kit';
+
+import style from './Properties.module.scss';
+
 export const Properties: FC = (): JSX.Element => {
+  const { control } = useForm();
+  const categories = useAppSelector(state => state.common.categories);
+  const nameData = categories ? categories.filter(c => c.level === 1) : [];
+
   return (
-    <div>
-      <p>Coming soon..</p>
-    </div>
+    <form>
+      <div className={style.container}>
+        <Label label="Ocassion" htmlFor="ocassion" className={style.label}>
+          <Controller
+            name="ocassion"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Select"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+        <Label label="Season" htmlFor="season" className={style.label}>
+          <Controller
+            name="season"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Select"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+        <Label label="Sizes grid type" htmlFor="sizesGrid" className={style.label}>
+          <Controller
+            name="sizesGrid"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Select"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+        <Label label="Gender" htmlFor="gender" className={style.label}>
+          <Controller
+            name="gender"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Select"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+        <Label label="Material (optional)" htmlFor="material" className={style.label}>
+          <Controller
+            name="material"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Enter the material name"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+        <Label label="% (optional)" htmlFor="percentage" className={style.label}>
+          <Controller
+            name="percentage"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder="Enter percentage of material"
+                className={style.percentage}
+              />
+            )}
+          />
+        </Label>
+        <button type="button" className={style.button}>
+          + Add material
+        </button>
+      </div>
+    </form>
   );
 };
