d8879445f28cd65528693cc48fbf78b36e5ebdfa
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-12-23 22:09:18 +0300

    Added: Variation

--- src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Variation/Variation.tsx
+++ src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Variation/Variation.tsx
@@ -1,9 +1,54 @@
 import React, { FC } from 'react';
 
+import { Controller, useForm } from 'react-hook-form';
+
+import { useAppSelector } from 'common/hooks';
+import { Label, Select } from 'ui-kit';
+
+import style from './Variation.module.scss';
+
 export const Variation: FC = (): JSX.Element => {
+  const { control } = useForm();
+  const categories = useAppSelector(state => state.common.categories);
+  const nameData = categories ? categories.filter(c => c.level === 1) : [];
+
   return (
-    <div>
-      <p>Coming soon..</p>
-    </div>
+    <form>
+      <div className={style.container}>
+        <div className={style.button_container}>
+          <button type="button" className={style.button}>
+            Var 1
+          </button>
+          <button type="button" className={style.button}>
+            Var 2
+          </button>
+          <button type="button" className={style.plus_button}>
+            +
+          </button>
+        </div>
+        <Label label="Variation type" htmlFor="variation" className={style.label}>
+          <Controller
+            name="variation"
+            control={control}
+            render={({ field }) => (
+              <div className={style.select_container}>
+                <Select
+                  {...field}
+                  placeholder="Select variation type"
+                  className={style.select_input}
+                  options={nameData.map(el => ({
+                    value: el.id,
+                    label: { text: el.name },
+                  }))}
+                  onChange={value => {
+                    field.onChange(String(value.value));
+                  }}
+                />
+              </div>
+            )}
+          />
+        </Label>
+      </div>
+    </form>
   );
 };
