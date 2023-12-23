fe289ee3befa3908893806ade846eeeb7bfdc6c5
sashaChumasov <sashachumasov@yahoo.com>
2023-12-23 13:32:09 +0300

    feat: deleted comments

--- src/pages/general-pages/ProductListPage/ProductFilter/Brand/Brand.tsx
+++ src/pages/general-pages/ProductListPage/ProductFilter/Brand/Brand.tsx
@@ -1,12 +1,9 @@
-import { Search } from 'ui-kit';
-
 import style from './Brand.module.scss';
 
 export const Brand = (): JSX.Element => {
   return (
     <div className={style.brand}>
       <p className={style.title}>Brand</p>
-      {/* <Search placeholder="Search" /> */}
     </div>
   );
 };
