767bbae7c3d78ebc74074dbfd653caccecbab521
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-12-26 11:37:35 +0300

    Added: new component SelectField

--- src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Properties/Properties.tsx
+++ src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Properties/Properties.tsx
@@ -2,16 +2,14 @@ import React, { FC, useState } from 'react';
 
 import { Controller, useForm } from 'react-hook-form';
 
-import { useAppSelector } from 'common/hooks';
-import { Label, Input, Select } from 'ui-kit';
+import { SelectField } from './SelectField/SelectField';
+
+import { Label, Input } from 'ui-kit';
 
 import style from './Properties.module.scss';
 
 export const Properties: FC = (): JSX.Element => {
   const { control, watch } = useForm();
-  const categories = useAppSelector(state => state.common.categories);
-  const nameData = categories ? categories.filter(c => c.level === 1) : [];
-
   const [showAdditional, setShowAdditional] = useState(false);
 
   const selectedMaterial = watch('material');
@@ -19,116 +17,15 @@ export const Properties: FC = (): JSX.Element => {
   return (
     <form>
       <div className={style.container}>
-        <Label label="Ocassion" htmlFor="ocassion" className={style.label}>
-          <Controller
-            name="ocassion"
-            control={control}
-            render={({ field }) => (
-              <div className={style.select_container}>
-                <Select
-                  {...field}
-                  placeholder="Select"
-                  className={style.select_input}
-                  options={nameData.map(el => ({
-                    value: el.id,
-                    label: { text: el.name },
-                  }))}
-                  onChange={value => {
-                    field.onChange(String(value.value));
-                  }}
-                />
-              </div>
-            )}
-          />
-        </Label>
-        <Label label="Season" htmlFor="season" className={style.label}>
-          <Controller
-            name="season"
-            control={control}
-            render={({ field }) => (
-              <div className={style.select_container}>
-                <Select
-                  {...field}
-                  placeholder="Select"
-                  className={style.select_input}
-                  options={nameData.map(el => ({
-                    value: el.id,
-                    label: { text: el.name },
-                  }))}
-                  onChange={value => {
-                    field.onChange(String(value.value));
-                  }}
-                />
-              </div>
-            )}
-          />
-        </Label>
-        <Label label="Sizes grid type" htmlFor="sizesGrid" className={style.label}>
-          <Controller
-            name="sizesGrid"
-            control={control}
-            render={({ field }) => (
-              <div className={style.select_container}>
-                <Select
-                  {...field}
-                  placeholder="Select"
-                  className={style.select_input}
-                  options={nameData.map(el => ({
-                    value: el.id,
-                    label: { text: el.name },
-                  }))}
-                  onChange={value => {
-                    field.onChange(String(value.value));
-                  }}
-                />
-              </div>
-            )}
-          />
-        </Label>
-        <Label label="Gender" htmlFor="gender" className={style.label}>
-          <Controller
-            name="gender"
-            control={control}
-            render={({ field }) => (
-              <div className={style.select_container}>
-                <Select
-                  {...field}
-                  placeholder="Select"
-                  className={style.select_input}
-                  options={nameData.map(el => ({
-                    value: el.id,
-                    label: { text: el.name },
-                  }))}
-                  onChange={value => {
-                    field.onChange(String(value.value));
-                  }}
-                />
-              </div>
-            )}
-          />
-        </Label>
-        <Label label="Material (optional)" htmlFor="material" className={style.label}>
-          <Controller
-            name="material"
-            control={control}
-            render={({ field }) => (
-              <div className={style.select_container}>
-                <Select
-                  {...field}
-                  placeholder="Enter the material name"
-                  className={style.select_input}
-                  options={nameData.map(el => ({
-                    value: el.id,
-                    label: { text: el.name },
-                  }))}
-                  onChange={value => {
-                    field.onChange(String(value.value));
-                  }}
-                />
-              </div>
-            )}
-          />
-        </Label>
+        <SelectField name="ocassion" label="Ocassion" />
+        <SelectField name="season" label="Season" />
+        <SelectField name="sizesGrid" label="Sizes grid type" />
+        <SelectField name="gender" label="Gender" />
+        <SelectField
+          name="material"
+          label="Material (optional)"
+          placeholder="Enter the material name"
+        />
         <Label label="% (optional)" htmlFor="percentage" className={style.label}>
           <Controller
             name="percentage"
@@ -153,28 +50,11 @@ export const Properties: FC = (): JSX.Element => {
       </div>
       {showAdditional && (
         <div className={style.additional}>
-          <Label label="Add material" htmlFor="addMaterial" className={style.label}>
-            <Controller
-              name="addMaterial"
-              control={control}
-              render={({ field }) => (
-                <div className={style.select_container}>
-                  <Select
-                    {...field}
-                    placeholder="Enter the material name"
-                    className={style.select_input}
-                    options={nameData.map(el => ({
-                      value: el.id,
-                      label: { text: el.name },
-                    }))}
-                    onChange={value => {
-                      field.onChange(String(value.value));
-                    }}
-                  />
-                </div>
-              )}
-            />
-          </Label>
+          <SelectField
+            name="addMaterial"
+            label="Add material"
+            placeholder="Enter the material name"
+          />
           <Label label="% (optional)" htmlFor="percentage" className={style.label}>
             <Controller
               name="percentage"
---
+++ src/pages/supplier-pages/pages/SupplierProducts/ProductsList/AddNewProductPage/Properties/SelectField/SelectField.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+
+import { Controller, useForm } from 'react-hook-form';
+
+import { useAppSelector } from 'common/hooks';
+import { Label, Select } from 'ui-kit';
+
+import style from './Select.module.scss';
+
+interface SelectFieldProps {
+  name: string;
+  label: string;
+  placeholder?: string;
+}
+
+export const SelectField: FC<SelectFieldProps> = ({ name, label, placeholder }) => {
+  const { control } = useForm();
+  const categories = useAppSelector(state => state.common.categories);
+  const nameData = categories ? categories.filter(c => c.level === 1) : [];
+
+  return (
+    <Label label={label} htmlFor={name} className={style.label}>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <div className={style.select_container}>
+            <Select
+              {...field}
+              placeholder={placeholder || 'Select'}
+              className={style.select_input}
+              options={nameData.map(el => ({
+                value: el.id,
+                label: { text: el.name },
+              }))}
+              onChange={value => {
+                field.onChange(String(value.value));
+              }}
+            />
+          </div>
+        )}
+      />
+    </Label>
+  );
+};
