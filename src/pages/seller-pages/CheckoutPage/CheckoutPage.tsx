026a685ea8a5b6b34df74bd87e3371b7f9810065
Yelkira <penda.fu@gmail.com>
2024-04-28 02:43:07 +0300

    feat: add new info blocks and styles for them

--- src/pages/seller-pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/seller-pages/CheckoutPage/CheckoutPage.tsx
@@ -1,15 +1,18 @@
 import React, { JSX, useCallback, useEffect } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { AddressAdder } from './AdressAdder/AdressAdder';
 import { PaymentMethodAdder } from './PaymentMethodAdder/PaymentMethodAdder';
 
+import { LockIcon } from 'assets/icons';
 import { WithLayout } from 'common/hocs/WithLayout';
 import { useAppDispatch, useAppSelector } from 'common/hooks';
 import { OrderDetails } from 'pages/seller-pages/SellerCart/CartWithOrder/OrderDetails';
 import { OrderItemsSection } from 'pages/seller-pages/SellerCart/CartWithOrder/OrderItemsSection';
 import { getSellerCartData, productsInCart } from 'store/reducers/seller/cart';
 import { isLoading } from 'store/reducers/seller/cart/selectors';
-import { ButtonQuestion, LoaderLinear } from 'ui-kit';
+import { ButtonQuestion, LoaderLinear, Paragraph } from 'ui-kit';
 
 import styles from './CheckoutPage.module.scss';
 
@@ -46,14 +49,36 @@ export const CheckoutPage = WithLayout((): JSX.Element => {
           <PaymentMethodAdder />
 
           <div className={styles.order_info}>
-            <div className={styles.order_title}>Items to Order</div>
+            <span className={styles.order_title}>Items to Order</span>
 
             {productsCart.map((products, index) => (
               <OrderItemsSection isCheckoutPage products={products} key={index} />
             ))}
           </div>
         </div>
-        <OrderDetails ordersId={ordersId as number[]} getCartData={getCartData} />
+        <div className={styles.right_column}>
+          <OrderDetails
+            ordersId={ordersId as number[]}
+            getCartData={getCartData}
+            additionalClassName={styles.order}
+          >
+            <Paragraph size="s2" className={styles.order_security}>
+              <LockIcon />
+              Your data and orders are secured
+            </Paragraph>
+          </OrderDetails>
+          <div className={styles.info_block}>
+            By placing an order, you agree to the{' '}
+            <Link to="/terms_and_conditions" className={styles.link}>
+              Terms & Conditions
+            </Link>{' '}
+            of the service and the{' '}
+            <Link to="/privacy_policy" className={styles.link}>
+              Privacy Policy
+            </Link>
+            .
+          </div>
+        </div>
       </div>
 
       <ButtonQuestion />
