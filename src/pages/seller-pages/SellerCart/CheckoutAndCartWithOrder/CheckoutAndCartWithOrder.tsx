ceb2e97c8c8e269845008709153cba7066782de6
Yelkira <penda.fu@gmail.com>
2024-05-14 03:26:36 +0300

    feat: Implement CheckoutAndCartWithOrder component that renders CartWithOrder or CheckoutPage based on the toggle state

---
+++ src/pages/seller-pages/SellerCart/CheckoutAndCartWithOrder/CheckoutAndCartWithOrder.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+import { CheckoutPage } from 'pages/seller-pages/CheckoutPage/CheckoutPage';
+import { CartWithOrder } from 'pages/seller-pages/SellerCart/CartWithOrder/CartWithOrder';
+
+const CheckoutAndCartWithOrder = (): JSX.Element => {
+  const [toggle, setToggle] = useState(false);
+
+  return !toggle ? <CartWithOrder handleButton={setToggle} /> : <CheckoutPage />;
+};
+
+export default CheckoutAndCartWithOrder;
