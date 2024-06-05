4261a8329d3c657c2f1a624f3d6aafc4bd7e0b76
Yelkira <penda.fu@gmail.com>
2024-06-05 12:59:25 +0300

    feat: added new logic and styles for displaying in Bundles

--- src/ui-kit/product-params/ProductColor/ProductColor.tsx
+++ src/ui-kit/product-params/ProductColor/ProductColor.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, HTMLAttributes } from 'react';
+import React, { forwardRef, HTMLAttributes, useState } from 'react';
 
 import cn from 'classnames';
 
@@ -8,14 +8,74 @@ interface IProductColor extends HTMLAttributes<HTMLDivElement> {
   value: number;
   imageUrl: string;
   className?: string;
+  handleSelectColorOrSize?: (id: number, quantity: number) => void;
+  selectedId?: { id: number }[];
+  isBundles?: boolean;
+  colorId?: number;
 }
 
 export const ProductColor = forwardRef<HTMLDivElement, IProductColor>(
-  ({ imageUrl, value, className }, ref): JSX.Element => {
+  (
+    {
+      imageUrl,
+      value,
+      className,
+      isBundles,
+      handleSelectColorOrSize,
+      selectedId,
+      colorId,
+    },
+    ref,
+  ): JSX.Element => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [newQuantity, setNewQuantity] = useState(value);
+
+    const isSelected = selectedId?.some(selected => selected.id === colorId);
+
+    const amount = cn(style.amount, {
+      [style.selected_amount]: isSelected,
+    });
+    const color = cn(style.color_image, {
+      [style.selected]: isSelected,
+    });
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setNewQuantity(Number(event.target.value));
+    };
+
+    if (!isBundles) {
+      return (
+        <div className={cn(style.item, className)} ref={ref}>
+          <img className={style.color_image} src={imageUrl} alt="color" />
+          <span className={style.amount}>{value}</span>
+        </div>
+      );
+    }
+
     return (
-      <div className={cn(style.item, className)} ref={ref}>
-        <img className={style.color_image} src={imageUrl} alt="color" />
-        <span className={style.amount}>{value}</span>
+      // @ts-ignore
+      // eslint-disable-next-line
+      <div
+        className={cn(style.item_bundles, className)}
+        ref={ref}
+        onClick={() => handleSelectColorOrSize(colorId, newQuantity)}
+      >
+        <img className={color} src={imageUrl} alt="color" />
+        {/* eslint-disable-next-line */}
+        <span onClick={() => setIsOpen(true)} className={amount}>
+          {isOpen ? (
+            <input
+              /* eslint-disable-next-line jsx-a11y/no-autofocus */
+              autoFocus
+              type="number"
+              onBlur={() => setIsOpen(false)}
+              onChange={handleOnChange}
+              className={style.input}
+              value={newQuantity}
+            />
+          ) : (
+            newQuantity
+          )}
+        </span>
       </div>
     );
   },
--- src/ui-kit/product-params/ProductSize/ProductSize.tsx
+++ src/ui-kit/product-params/ProductSize/ProductSize.tsx
@@ -1,18 +1,62 @@
-import React, { forwardRef, HTMLAttributes } from 'react';
+import React, { forwardRef, HTMLAttributes, useState } from 'react';
 
 import style from './ProductSize.module.scss';
 
 interface IProductSize extends HTMLAttributes<HTMLDivElement> {
   size: string;
   quantity: number;
+  sizeId?: number;
+  handleSelectColorOrSize?: (id: number, quantity: number) => void;
+  selectedId?: { id: number }[];
+  isBundles?: boolean;
 }
 
 export const ProductSize = forwardRef<HTMLDivElement, IProductSize>(
-  ({ size, quantity }, ref) => {
+  ({ size, quantity, sizeId, handleSelectColorOrSize, selectedId, isBundles }, ref) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [newQuantity, setNewQuantity] = useState(quantity);
+
+    const isSelected = selectedId?.some(selected => selected.id === sizeId);
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setNewQuantity(Number(event.target.value));
+    };
+
+    if (!isBundles) {
+      return (
+        <div ref={ref}>
+          <div className={style.size}>{size}</div>
+          <div className={style.quantity}>{quantity}</div>
+        </div>
+      );
+    }
+
     return (
-      <div ref={ref}>
-        <div className={style.size}>{size}</div>
-        <div className={style.quantity}>{quantity}</div>
+      // @ts-ignore
+      // eslint-disable-next-line
+      <div ref={ref} onClick={() => handleSelectColorOrSize(sizeId, newQuantity)}>
+        <div className={`${style.size_bundles} ${isSelected && style.selected_size}`}>
+          {size}
+        </div>
+        {/* eslint-disable-next-line */}
+        <div
+          onClick={() => setIsOpen(true)}
+          className={`${style.quantity_bundles} ${isSelected && style.selected_quantity}`}
+        >
+          {isOpen ? (
+            <input
+              /* eslint-disable-next-line jsx-a11y/no-autofocus */
+              autoFocus
+              type="number"
+              onBlur={() => setIsOpen(false)}
+              onChange={handleOnChange}
+              className={style.input}
+              value={newQuantity}
+            />
+          ) : (
+            newQuantity
+          )}
+        </div>
       </div>
     );
   },
