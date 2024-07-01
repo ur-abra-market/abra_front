a7dd18336846e621b040d097392037be106620b3
alexandrIvanovDev <92260080+alexandrIvanovDev@users.noreply.github.com>
2024-07-01 16:26:09 +0300

    Fix/error message on login and register page (#424)

--- src/store/reducers/authSlice/thunks.ts
+++ src/store/reducers/authSlice/thunks.ts
@@ -35,7 +35,7 @@ export const registerUser = createAsyncThunk<void, IRegisterRequest, IAsyncThunk
         dispatch(
           setResponseNotice({
             noticeType: 'error',
-            message: error.response?.data?.error || error.message,
+            message: error.response?.data?.detail || error.message,
           }),
         );
       }
@@ -96,7 +96,7 @@ export const loginUser = createAsyncThunk<
       dispatch(
         setResponseNotice({
           noticeType: 'error',
-          message: error.response?.data?.error || error.message,
+          message: error.response?.data?.detail || error.message,
         }),
       );
     }
